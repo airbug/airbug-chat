airbug/airbug-chat

commit 9cac0e18749fb42ddc0ab4f5c2172a90dcd72ea1

Fleshes out most of the MVPChatBasics controller/routes and underlying services and managers

projects/airbugserver/js/src/managers/RoomManager.js
@@ -7,7 +7,6 @@
 //@Export('RoomManager')
 
 //@Require('Class')
-//@Require('Proxy')
 //@Require('airbugserver.BugManager')
 //@Require('bugflow.BugFlow')
 
@@ -24,7 +23,6 @@ var bugpack     = require('bugpack').context();
 //-------------------------------------------------------------------------------
 
 var Class       = bugpack.require('Class');
-var Proxy       = bugpack.require('Proxy');
 var BugManager  = bugpack.require('airbugserver.BugManager');
 var BugFlow     = bugpack.require('bugflow.BugFlow');
 
@@ -43,13 +41,9 @@ var $task       = BugFlow.$task;
 
 var RoomManager = Class.extend(BugManager, {
 
-    //-------------------------------------------------------------------------------
-    // Constructor
-    //-------------------------------------------------------------------------------
+    _constructor: function(model, schema, conversationManager, roomMemberManager){
 
-    _constructor: function(model, schema, roomMemberManager) {
-
-        this._super(model, schema);
+        this._super(model, shema);
 
 
         //-------------------------------------------------------------------------------
@@ -57,70 +51,76 @@ var RoomManager = Class.extend(BugManager, {
         //-------------------------------------------------------------------------------
 
         /**
-         * @private
-         * @type {RoomMemberManager}
+         * @type {airbugserver.ConversationManager}
          */
-        this.roomMemberManager = roomMemberManager;
+        this.conversationManager    = conversationManager;
+
+        /**
+         * @type {airbugserver.RoomMemberManager}
+         */
+        this.roomMemberManager      = roomMemberManager;
+
     },
 
     //-------------------------------------------------------------------------------
     // Instance Methods
     //-------------------------------------------------------------------------------
 
+    /**
+     * @override
+     */
     configure: function(callback){
-        if(!callback || typeof callback !== 'function') var callback = function(){};
+        if (!callback || typeof callback !== 'function') var callback = function(){};
+        var _this = this;
 
-
-        this.pre('save', function (next){
-            if (!this.createdAt) this.createdAt = new Date();
+        this.pre('save', true, function(next, done){
             next();
+            if (!this.createdAt) this.createdAt = new Date();
+            done();
         });
-        this.pre('save', function(next){
+        this.pre('save', true, function(next, done){
+            next();
             this.updatedAt = new Date();
+            done();
         });
+
+        this.post('save', function(next){
+            if (!this.conversationId) {
+                var conversation        = _this.conversationManager.new();
+                var conversationId      = conversation.id;
+                conversation.ownerId    = this.id;
+                conversation.save(function(error, conversation){
+                    if (!error && conversation){
+                        next();
+                    } else {
+                        next(error);
+                    }
+                })
+            }
+        })
 
         callback();
     },
 
-    getModel: function(){
-        return this.model;
-    },
-
-    getSchema: function(){
-        return this.schema;
-    },
-
+    /**
+     * @param {} roomId
+     * @param {function(error, membersList)} callback
+     */
     getMembersList: function(roomId, callback){
-        this.model.findById(roomId, function(error, room){
-            return room.memberslist;
+        this.findById(roomId, function(error, room){
+            callback(error, room.membersList);
         });
     },
 
-    getMembers: function(roomId, callback){
-        // var _this = this;
-        // this.model.findById(roomId, function(error, room){
-        //     if(!error){
-        //         var opts = {path: 'roomMember', model: 'RoomMember'};
-        //         _this.roomMemberManager.populate(room, opts, function(error, room){
-        //             if(!error){
-        //                 room.membersList.remove({userId: userId}); // Test this
-        //                 room.save(callback);
-        //             } else {
-        //                 callback(error);
-        //             }
-        //         });
-        //     } else {
-        //         callback(error);
-        //     }
-        // });    
-    },
-
     /**
-     * @param {} room
-     * @param {function(error, room)} callback
+     * @param {} roomId
+     * @param {function(error, members)} callback
      */
-    create: function(room, callback){
-        this.model.create(room, callback);
+    getMembers: function(roomId, callback){
+        var _this = this;
+        this.findById(roomId).populate("membersList").exec(function(error, room){
+            callback(error, room.membersList);
+        });   
     },
 
     /**
@@ -130,18 +130,18 @@ var RoomManager = Class.extend(BugManager, {
      */
     addUser: function(roomId, userId, callback){
         var _this = this;
-        this.model.findById(roomId, function(error, room){
-            if(!error){
+        this.findById(roomId, function(error, room){
+            if(!error && room){
                 _this.roomMemberManager.create({userId: userId}, function(error, roomMember){
                     if(!error && roomMember){
-                        room.membersList.push(roomMember);
+                        room.membersList.push(roomMember); //What happens if I push the entire object instead of just the id???
                         room.save(callback);
                     } else {
                         callback(error);
                     }
                 });
             } else {
-                callback(error);
+                callback(error, room);
             }
         })
     },
@@ -154,47 +154,20 @@ var RoomManager = Class.extend(BugManager, {
     removeRoomMember: function(roomId, roomMemberId, callback){
         var _this = this;
         var room;
-        var roomMemberId;
-        $series([
-            $task(function(flow){
-                _this.model.findById(roomId, function(error, returnedRoom){
-                    if(!error) room = returnedRoom;
-                    flow.complete(error);
-                });
-            }),
-            $task(function(flow){
-                room.membersList.remove(roomMemberId);
-                room.save(function(error, room){
-                    flow.complete(error);
-                });
-            })
-        ]).execute(callback);
-    },
-
-    /**
-     * @param {} roomId
-     * @param {} userId
-     * @param {function(error)} callback
-     */
-    removeUser: function(roomId, userId, callback){
-        var _this = this;
-        var room;
-        var roomMember;
-        var roomMemberId;
         $series([
             $parallel([
                 $task(function(flow){
-                    _this.model.findById(roomId, function(error, returnedRoom){
+                    _this.findById(roomId, function(error, returnedRoom){
                         if(!error && room) room = returnedRoom;
                         flow.complete(error);
                     });
                 }),
                 $task(function(flow){
-                    _this.roomMemberManager.find({roomId: roomId, userId: userId}, function(error, roomMember){
+                    _this.roomMemberManager.findById(roomMemberId, function(error, roomMember){
                         if(!error && roomMember) {
-                            roomMember.remove(function(error, product){
+                            roomMember.remove(function(error){
                                 flow.complete(error);
-                            })
+                            });
                         } else {
                             flow.complete(error);
                         }
@@ -209,7 +182,50 @@ var RoomManager = Class.extend(BugManager, {
                     });
                 })
             ])
-        ]).execute(callback);
+        ]).execute(function(error){
+            callback(error, room);
+        });
+    },
+
+    /**
+     * @param {} roomId
+     * @param {} userId
+     * @param {function(error, room)} callback
+     */
+    removeUser: function(roomId, userId, callback){
+        var _this = this;
+        var room;
+        $series([
+            $parallel([
+                $task(function(flow){
+                    _this.findById(roomId, function(error, returnedRoom){
+                        if(!error && room) room = returnedRoom;
+                        flow.complete(error);
+                    });
+                }),
+                $task(function(flow){
+                    _this.roomMemberManager.findOne({roomId: roomId, userId: userId}, function(error, roomMember){
+                        if(!error && roomMember) {
+                            roomMember.remove(function(error){
+                                flow.complete(error);
+                            });
+                        } else {
+                            flow.complete(error);
+                        }
+                    });
+                })
+            ]),
+            $parallel([
+                $task(function(flow){
+                    room.membersList.remove(roomMemberId);
+                    room.save(function(error, room){
+                        flow.complete(error);
+                    });
+                })
+            ])
+        ]).execute(function(error){
+            callback(error, room);
+        });
     }
 });
 

projects/airbugserver/js/src/AirbugServerConfiguration.js
@@ -34,6 +34,9 @@
 //@Require('airbugserver.RoomController')
 //@Require('airbugserver.UserController')
 
+//@Require('airbugserver.ChatMessageService')
+//@Require('airbugserver.ConnectionService')
+//@Require('airbugserver.ConversationService')
 //@Require('airbugserver.RoomService')
 //@Require('airbugserver.SessionService')
 //@Require('airbugserver.UserService')
@@ -105,8 +108,11 @@ var HomePageController      = bugpack.require('airbugserver.HomePageController')
 var RoomController          = bugpack.require('airbugserver.RoomController');
 var UserController          = bugpack.require('airbugserver.UserController');
 
-var SessionService          = bugpack.require('airbugserver.SessionService');
+var ChatMessageService      = bugpack.require('airbugserver.ChatMessageService');
+var ConnectionService       = bugpack.require('airbugserver.ConnectionService')
+var ConversationService     = bugpack.require('airbugserver.ConversationService');
 var RoomService             = bugpack.require('airbugserver.RoomService');
+var SessionService          = bugpack.require('airbugserver.SessionService');
 var UserService             = bugpack.require('airbugserver.UserService');
 
 var ChatMessageManager      = bugpack.require('airbugserver.ChatMessageManager');
@@ -175,6 +181,12 @@ var AirbugServerConfiguration = Class.extend(Obj, {
 
         /**
          * @private
+         * @type {ChatMessageService}
+         */
+        this._chatMessageService    = null;
+
+        /**
+         * @private
          * @type {{
          *      port: number,
          *      mongoDbIp: string
@@ -190,9 +202,21 @@ var AirbugServerConfiguration = Class.extend(Obj, {
 
         /**
          * @private
+         * @type {ConnectionService}
+         */
+        this._connectionService     = null;
+
+        /**
+         * @private
          * @type {ConversationManager}
          */
         this._conversationManager   = null;
+
+        /**
+         * @private
+         * @type {ConversationService}
+         */
+        this._conversationService   = null;
 
         /**
          * @private
@@ -229,6 +253,12 @@ var AirbugServerConfiguration = Class.extend(Obj, {
          * @type {RoomManager}
          */
         this._roomManager           = null;
+
+        /**
+         * @private
+         * @type {RoomService}
+         */
+        this._roomService           = null;
 
         /**
          * @private
@@ -505,11 +535,29 @@ var AirbugServerConfiguration = Class.extend(Obj, {
     },
 
     /**
+     * @param {ChatMessageManager} chatMessageManager
+     * @return {ChatMessageService}
+     */
+    chatMessageService: function(chatMessageManager) {
+        this._chatMessageService = new ChatMessageService(chatMessageManager);
+        return this._chatMessageService;
+    },
+
+    /**
      * @return {Object}
      */
     config: function() {
         this._config = this.loadConfig(this._configFilePath);
         return this._config;
+    },
+
+    /**
+     * @param {BugCallServer} bugCallServer
+     * @return {ConnectionService}
+     */
+    connectionService: function(bugCallServer){
+        this._connectionService = new ConnectionService(bugCallServer);
+        return this._connectionService;
     },
 
     /**
@@ -527,6 +575,15 @@ var AirbugServerConfiguration = Class.extend(Obj, {
     conversationManager: function(model, schema) {
         this._conversationManager = new ConversationManager(model, schema);
         return this._conversationManager;
+    },
+
+    /**
+     * @param {ConversationManager} conversationManager
+     * @return {ConversationService}
+     */
+    conversationService: function(conversationManager){
+        this._conversationService = new ConversationService(conversationManager);
+        return this._conversationService;
     },
 
     /**
@@ -640,8 +697,8 @@ var AirbugServerConfiguration = Class.extend(Obj, {
      * @param {RoomService} roomService
      * @return {RoomController}
      */
-    roomController: function(bugCallRouter, roomService) {
-        this._roomController = new RoomController(bugCallRouter, roomService);
+    roomController: function(socketRouter, roomService, connectionService) {
+        this._roomController = new RoomController(socketRouter, roomService, connectionService);
         return this._roomController;
     },
 
@@ -658,7 +715,8 @@ var AirbugServerConfiguration = Class.extend(Obj, {
      * @return {RoomService}
      */
     roomService: function(roomManager, userManager) {
-        return new RoomService(roomManager, userManager);
+        this._roomService = new RoomService(roomManager, userManager);
+        return this._roomService;
     },
 
     /**
@@ -896,12 +954,44 @@ annotate(AirbugServerConfiguration).with(
         module("roomController")
             .args([
                 arg("bugCallRouter").ref("bugCallRouter"),
-                arg("roomService").ref("roomService")
+                arg("roomService").ref("roomService"),
+                arg("connectionService").ref("connectionService")
             ]),
         module("userController")
             .args([
                 arg("bugCallRouter").ref("bugCallRouter"),
                 arg("userService").ref("userService")
+            ]),
+
+
+       //-------------------------------------------------------------------------------
+        // Services
+        //-------------------------------------------------------------------------------
+
+        module("chatMessageService")
+            .args([
+                arg("chatMessageManager").ref("chatMessageManager")
+            ]),
+        module("connectionService")
+            .args([
+                arg("bugCallServer").ref("bugCallServer")
+            ]),
+        module("conversationService")
+            .args([
+                arg("conversationManager").ref("conversationManager")
+            ]),
+        module("roomService")
+            .args([
+                arg("roomManager").ref("roomManager"),
+                arg("userManager").ref("userManager")
+            ]),
+        module("sessionService")
+            .args([
+                arg("sessionManager").ref("sessionManager")
+            ]),
+        module("userService")
+            .args([
+                arg("userManager").ref("userManager")
             ]),
 
 
@@ -938,25 +1028,6 @@ annotate(AirbugServerConfiguration).with(
             .args([
                 arg("model").ref("user"),
                 arg("schema").ref("userSchema")
-            ]),
-
-
-        //-------------------------------------------------------------------------------
-        // Services
-        //-------------------------------------------------------------------------------
-
-        module("roomService")
-            .args([
-                arg("roomManager").ref("roomManager"),
-                arg("userManager").ref("userManager")
-            ]),
-        module("sessionService")
-            .args([
-                arg("sessionManager").ref("sessionManager")
-            ]),
-        module("userService")
-            .args([
-                arg("userManager").ref("userManager")
             ]),
 
 

projects/airbugserver/js/src/Schemas/ChatMessageSchema.js
@@ -29,8 +29,10 @@ var Mixed       = mongoose.Schema.Types.Mixed;
 //-------------------------------------------------------------------------------
 
 var ChatMessageSchema = new Schema({
-    senderUserId: ObjectId,
-    messageBody: String,
+	conversationOwnerId: {type: ObjectId, require: true},
+	conversationId: {type: ObjectId, require: true},
+    senderUserId: {type: ObjectId, require: true},
+    messageBody: {type: String, require: true},
     createdAt: Date,
     updatedAt: Date
 });

projects/airbugserver/js/src/Schemas/ConversationSchema.js
@@ -29,7 +29,7 @@ var Mixed       = mongoose.Schema.Types.Mixed;
 //-------------------------------------------------------------------------------
 
 var ConversationSchema = new Schema({
-    ownerId: ObjectId,
+    ownerId: {type: ObjectId, require: true}, //NOTE: Room or Dialogue
     messageIdList: [ObjectId],
     messageList: [Mixed], //type: ChatMessage
     createdAt: Date,

projects/airbugserver/js/src/Schemas/RoomSchema.js
@@ -31,7 +31,7 @@ var ObjectId    = mongoose.Schema.Types.ObjectId;
 var RoomSchema = new Schema({
     name: String,
     conversationId: ObjectId,
-    membersList: [ObjectId],
+    membersList: [{ type: ObjectId, ref: 'RoomMember' }],
     createdAt: Date,
     updatedAt: Date
 });

projects/airbugserver/js/src/Schemas/UserSchema.js
@@ -32,9 +32,27 @@ var UserSchema = new Schema({
     firstName: String,
     lastName: String,
     anonymous: Boolean,
-    roomsList: [ObjectId],
+    roomsList: [{type: ObjectId, ref: 'Room'}],
     createdAt: Date,
     updatedAt: Date
+});
+
+//-------------------------------------------------------------------------------
+// Instance Methods
+//-------------------------------------------------------------------------------
+
+/**
+ * @return {boolean}
+ */
+UserSchema.method("isAnonymous", function(){
+	return this.anonymous;
+});
+
+/**
+ * @return {boolean}
+ */
+UserSchema.method("isNotAnonymous", function(){
+	return !this.anonymous;
 });
 
 

projects/airbugserver/js/src/controllers/ChatMessageController.js
@@ -4,7 +4,7 @@
 
 //@Package('airbugserver')
 
-//@Export('UserController')
+//@Export('ChatMessageController')
 
 //@Require('Class')
 //@Require('Obj')
@@ -29,13 +29,14 @@ var Obj         = bugpack.require('Obj');
 // Declare Class
 //-------------------------------------------------------------------------------
 
-var UserController = Class.extend(Obj, {
+var ChatMessageController = Class.extend(Obj, {
+
 
     //-------------------------------------------------------------------------------
     // Constructor
     //-------------------------------------------------------------------------------
 
-    _constructor: function(bugCallRouter, userService){
+    _constructor: function(bugCallRouter, chatMessageService){
 
         this._super();
 
@@ -46,82 +47,56 @@ var UserController = Class.extend(Obj, {
 
         /**
          * @private
-         * @type {BugCallRouter}
+         * @type {RoomService}
          */
-        this.bugCallRouter  = bugCallRouter;
+        this.chatMessageService     = chatMessageService;
 
         /**
          * @private
-         * @type {UserService}
+         * @type {BugCallRouter}
          */
-        this.userService    = userService;
+        this.bugCallRouter          = bugCallRouter;
     },
 
 
     //-------------------------------------------------------------------------------
-    // Public Instance Methods
+    // Methods
     //-------------------------------------------------------------------------------
 
     /**
      * @param {function(Error)} callback
      */
-    configure: function(callback){
+    configure: function(callback) {
         if(!callback || typeof callback !== 'function') var callback = function(){};
 
-        var _this = this;
+        var _this               = this;
         this.bugCallRouter.addAll({
 
             /**
              * @param {IncomingRequest} request
              * @param {CallResponder} responder
              */
-            establishUser:      function(request, responder){
+            createChatMessage: function(request, responder){
                 var currentUser = request.getHandshake().session.user;
-                if(currentUser.isAnonymous()){
-                    var data = request.getData();
-                    var user = data.user;
-                    _this.userService.establishUser(user, function(error, user){
-                        if(!error && user){
-                            var data = {user: user};
-                            var response = responder.response("establishedUser", data);
+                if(currentUser.isNotAnonymous()){
+                    var data        = request.getData();
+                    var chatMessage = data.chatMessage;
+                    _this.chatMessageService.createChatMessage(currentUser, chatMessage, function(error, chatMessage){
+                        if(!error && chatMessage){
+                            var data = {chatMessage: chatMessage};
+                            var response = responder.response("createdChatMessage", data);
                         } else {
                             var data = {error: error};
-                            var response = responder.response("establishUserError", data);
+                            var response = responder.response("createChatMessageError", data);
                         }
-                        responder.sendRespons(response);
-                    })
-                } else {
-                    //TODO
+                        responder.sendResponse(response);
+                    });
                 }
-            },
-
-            /**
-             * @param {IncomingRequest} request
-             * @param {CallResponder} responder
-             */
-            getCurrentUser:     function(request, responder){
-                var currentUser = request.getHandshake().session.user;
-                var data        = {currentUser: currentUser};
-                var response    = responder.response("gotCurrentUser", data);
-                responder.sendResponse(response);
-            },
-
-            /**
-             * @param {IncomingRequest} request
-             * @param {CallResponder} responder
-             */
-            logoutCurrentUser:  function(request, responder){
-                //TODO
-                var currentUser = request.getHandshake().session.user;
-                var connection  = request.getCallConnection();
-                this.connectionService.deregisterConnection(currentUser.id, connection);
-                this.userService.logoutUser(currentUser, function(error){
-
-                });
             }
         });
 
         callback();
+
     }
 });
 
@@ -130,4 +105,4 @@ var UserController = Class.extend(Obj, {
 // Exports
 //-------------------------------------------------------------------------------
 
-bugpack.export('airbugserver.UserController', UserController);
+bugpack.export('airbugserver.ChatMessageController', ChatMessageController);

projects/airbugserver/js/src/controllers/ConversationsController.js
@@ -4,15 +4,10 @@
 
 //@Package('airbugserver')
 
-//@Export('RoomMemberManager')
+//@Export('ConversationController')
 
 //@Require('Class')
-<<<<<<< Updated upstream
-=======
-//@Require('airbugserver.BugManager')
->>>>>>> Stashed changes
-//@Require('Proxy')
-//@Require('airbugserver.BugManager')
+//@Require('Obj')
 
 
 //-------------------------------------------------------------------------------
@@ -26,71 +21,83 @@ var bugpack     = require('bugpack').context();
 // Bugpack Modules
 //-------------------------------------------------------------------------------
 
-var BugManager  = bugpack.require('airbugserver.BugManager');
 var Class       = bugpack.require('Class');
-<<<<<<< Updated upstream
-var Proxy       = bugpack.require('Proxy');
-var BugManager  = bugpack.require('airbugserver.BugManager');
-=======
->>>>>>> Stashed changes
+var Obj         = bugpack.require('Obj');
 
 
 //-------------------------------------------------------------------------------
 // Declare Class
 //-------------------------------------------------------------------------------
 
-var RoomMemberManager = Class.extend(BugManager, {
+var ConversationController = Class.extend(Obj, {
+
 
     //-------------------------------------------------------------------------------
     // Constructor
     //-------------------------------------------------------------------------------
 
-    _constructor: function(model, schema, roomManager){
+    _constructor: function(bugCallRouter, conversationService){
 
-        this._super(model, schema);
+        this._super();
 
 
         //-------------------------------------------------------------------------------
-        // Properties
+        // Declare Variables
         //-------------------------------------------------------------------------------
 
         /**
-         * @type {RoomManager}
+         * @private
+         * @type {ConversationService}
          */
-        this.roomManager = roomManager;
+        this.conversationService    = conversationService;
 
+        /**
+         * @private
+         * @type {BugCallRouter}
+         */
+        this.bugCallRouter          = bugCallRouter;
     },
 
+
     //-------------------------------------------------------------------------------
-    // Instance Methods
+    // Methods
     //-------------------------------------------------------------------------------
 
     /**
-     * @override
-     * @param {function(error)} callback
+     * @param {function(Error)} callback
      */
-    configure: function(callback){
+    configure: function(callback) {
         if(!callback || typeof callback !== 'function') var callback = function(){};
 
-
-        this.pre('save', true, function(next, done){
-            next();
-            if (!this.createdAt) this.createdAt = new Date();
-            done();
-        });
-
-        this.pre('save', true, function(next, done){
-            next();
-            this.updatedAt = new Date();
-            done();
+        var _this               = this;
+        this.bugCallRouter.addAll({
+            retrieveConversation: function(request, responder){
+                var currentUser = request.getHandshake().session.user;
+                if(currentUser.isNotAnonymous()){
+                    var data = request.getData();
+                    var conversationId = data.conversationId;
+                    _this.conversationService.retrieveConversation(currentUser, conversationId, function(error, conversation){
+                        if(!error && conversation){
+                            var data = {conversation: conversation};
+                            var response = responder.response("retrievedConversation", data);
+                        } else {
+                            var data = {error: error};
+                            var response = responder.response("retrieveConversationError", data);
+                        }
+                        responder.sendResponse(response);
+                    });
+                }
+            }
         });
 
         callback();
+
     }
 });
+
 
 //-------------------------------------------------------------------------------
 // Exports
 //-------------------------------------------------------------------------------
 
-bugpack.export('airbugserver.RoomMemberManager', RoomMemberManager);
+bugpack.export('airbugserver.ConversationController', ConversationController);

commit b4d88c071cde198c5ce52865ee0f150d404e7158

Fixes a typo that causes server start failure introduced in 9cac0e18749fb42ddc0ab4f5c2172a90dcd72ea1

projects/airbugserver/js/src/managers/RoomManager.js
@@ -43,7 +43,7 @@ var RoomManager = Class.extend(BugManager, {
 
     _constructor: function(model, schema, conversationManager, roomMemberManager){
 
-        this._super(model, shema);
+        this._super(model, schema);
 
 
         //-------------------------------------------------------------------------------