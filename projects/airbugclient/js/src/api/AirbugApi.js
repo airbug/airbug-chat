c0cd2ba06f11401f7335e37a1fae61f3f30cea74
Sung Choi <m.sung.choi@gmail.com>
2013-06-20 13:41:42 -0700

    Flesh out airbugclient.AirbugApi to match routes on airbugserver

--- projects/airbugclient/js/src/api/AirbugApi.js
+++ projects/airbugclient/js/src/api/AirbugApi.js
@@ -55,17 +55,100 @@ var AirbugApi = Class.extend(Obj, {
 
 
     //-------------------------------------------------------------------------------
+    // Instance Methods
+    //-------------------------------------------------------------------------------
+
+    //-------------------------------------------------------------------------------
     // Getters and Setters
     //-------------------------------------------------------------------------------
 
 
+    //-------------------------------------------------------------------------------
+    // ChatMessage Related Api Methods
+    //-------------------------------------------------------------------------------
+
+    createChatMessage: function(chatMessageBody, conversationId, conversationOwnerId, callback){
+        var requestData = {
+            chatMessage: {
+                body: chatMessageBody,
+                conversationId: conversationId,
+                conversationOwnerId: conversationOwnerId
+            }
+        };
+        this.bugCallClient.request("createChatMessage", requestData, callback);
+    },
+
 
     //-------------------------------------------------------------------------------
-    // Class Methods
+    // Conversation Related Api Methods
     //-------------------------------------------------------------------------------
 
-    createUser: function(fullName, email, callback) {
+    retrieveConversation: function(conversationId, callback){
+        var requestData = {
+            conversationId: conversationId
+        };
+        this.bugCallClient.request("retrieveConversation", requestData, callback);
+    },
 
+
+    //-------------------------------------------------------------------------------
+    // Room Related Api Methods
+    //-------------------------------------------------------------------------------
+
+    addUserToRoom: function(userId, roomId, callback){
+        var requestData = {
+            roomId: roomId,
+            userId: userId
+        };
+        this.bugCallClient.request("addUserToRoom", requestData, callback);
+    },
+
+    createRoom: function(roomName, callback){
+        var requestData = {
+            room: {
+                name: roomName
+            }
+        };
+        this.bugCallClient.request("createRoom", requestData, callback);
+    },
+
+    joinRoom: function(roomId, callback){
+        var requestData = {
+            roomId: roomId
+        };
+        this.bugCallClient.request("joinRoom", requestData, callback);
+    },
+
+    leaveRoom: function(roomId, callback){
+        var requestData = {
+            roomId: roomId
+        };
+        this.bugCallClient.request("leaveRoom", requestData, callback);
+    },
+
+
+    //-------------------------------------------------------------------------------
+    // User Related Api Methods
+    //-------------------------------------------------------------------------------
+
+    getCurrentUser: function(callback){
+        this.bugCallClient.request("getCurrentUser", {}, callback);
+    },
+
+    establishCurrentUser: function(firstName, lastName, email, callback){
+        var requestData = {
+            user: {
+                firstName: firstName,
+                lastName: lastName,
+                email: email
+            }
+        };
+        this.bugCallClient.request("establishCurrentUser", requestData, callback);
+
+    },
+
+    logoutCurrentUser: function(callback){
+        this.bugCallClient.request("logoutCurrentUser", {}, callback);
     }
 });
 
