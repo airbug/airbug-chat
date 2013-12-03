6be394079a575512d634c4dc5c25534dd2e00a37
Dustin Kanske <dustin@kanske.com>
2013-12-02 14:29:35 -1000

    adding a few more assertions to user tests

--- projects/airbugserver/js/test/entities/UserTests.js
+++ projects/airbugserver/js/test/entities/UserTests.js
@@ -49,9 +49,12 @@ var userInstantiationTests = {
 
     // testPasswordHash will validate against password 'lastpass' no quotes.
     setup: function(test) {
+        this.testAnonymous = true;
         this.testPasswordHash = "$2a$10$UCNxW7UFww9z97eijL8QhewpxjqNCjv0CoPO/PKOyjdnMdoRSnlMe";
         this.testUser = new User({
+            anonymous: this.testAnonymous,
             passwordHash: this.testPasswordHash
+
         });
     },
 
@@ -60,9 +63,12 @@ var userInstantiationTests = {
     //-------------------------------------------------------------------------------
 
     test: function(test) {
+        test.assertEqual(this.testUser.getAnonymous(), this.testAnonymous,
+            "Assert User.anonymous was set correctly");
         test.assertEqual(this.testUser.getPasswordHash(), this.testPasswordHash,
             "Assert User.passwordHash was set correctly");
         test.complete();
+
     }
 };
 
@@ -80,6 +86,22 @@ var userGetterSetterTests = {
     //-------------------------------------------------------------------------------
 
     test: function(test) {
+        this.testUser.setAnonymous(true);
+        test.assertEqual(this.testUser.getAnonymous(), true,
+            "Assert User.setAnonymous works correctly when set to true");
+        test.assertTrue(this.testUser.isAnonymous(),
+            "Assert User.isAnonymous works correctly when set to true");
+        test.assertTrue(!this.testUser.isNotAnonymous(),
+            "Assert User.isNotAnonymous works correctly when set to true");
+
+        this.testUser.setAnonymous(false);
+        test.assertEqual(this.testUser.getAnonymous(), false,
+            "Assert User.setAnonymous works correctly when set to false");
+        test.assertTrue(!this.testUser.isAnonymous(),
+            "Assert User.isAnonymous works correctly when set to false");
+        test.assertTrue(this.testUser.isNotAnonymous(),
+            "Assert User.isNotAnonymous works correctly when set to false");
+
         this.testUser.setPasswordHash("testPasswordHash");
         test.assertEqual(this.testUser.getPasswordHash(), "testPasswordHash",
             "Assert User.setPasswordHash works correctly");
