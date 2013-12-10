7fd0559bf2de686818ab4242fc4f9336baf7690e
Sung Choi <m.sung.choi@gmail.com>
2013-12-10 01:05:08 -0800

    Update. Changes the icon for AddRoomMemberButtonContainer from share to plus and from white back to default black.

--- projects/airbugclient/js/src/containers/pages/roompage/addroommember/AddRoomMemberButtonContainer.js
+++ projects/airbugclient/js/src/containers/pages/roompage/addroommember/AddRoomMemberButtonContainer.js
@@ -118,7 +118,7 @@ var AddRoomMemberButtonContainer = Class.extend(ButtonContainer, {
                 .id("addRoomMemberButtonView")
                 .children([
                     view(IconView)
-                        .attributes({type: IconView.Type.SHARE, color: IconView.Color.WHITE})
+                        .attributes({type: IconView.Type.PLUS})
                         .appendTo('*[id|="button"]')
                 ])
                 .build();
