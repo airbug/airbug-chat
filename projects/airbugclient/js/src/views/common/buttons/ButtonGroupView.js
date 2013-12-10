64c511d0333ff9a3d223c296dbd0dd5cc686da4b
Sung Choi <m.sung.choi@gmail.com>
2013-12-10 14:24:05 -0800

    Adds ButtonGroupView file and class.

---
+++ projects/airbugclient/js/src/views/common/buttons/ButtonGroupView.js
@@ -0,0 +1,97 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbug')
+
+//@Export('ButtonGroupView')
+
+//@Require('Class')
+//@Require('airbug.ButtonViewEvent')
+//@Require('airbug.MustacheView')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// BugPack
+//-------------------------------------------------------------------------------
+
+var Class           = bugpack.require('Class');
+var ButtonViewEvent = bugpack.require('airbug.ButtonViewEvent');
+var MustacheView    = bugpack.require('airbug.MustacheView');
+
+
+//-------------------------------------------------------------------------------
+// Declare Class
+//-------------------------------------------------------------------------------
+
+/**
+ * @constructor
+ * @extends {MustacheView}
+ */
+var ButtonGroupView = Class.extend(MustacheView, {
+
+    //-------------------------------------------------------------------------------
+    // Constructor
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @constructs
+     * @param {Object} options
+     */
+    _constructor: function(options) {
+
+        this._super(options);
+
+
+        //-------------------------------------------------------------------------------
+        // Private Properties
+        //-------------------------------------------------------------------------------
+
+        if (!this.id) {
+            this.id = "button-group" + this.cid;
+        }
+    },
+
+
+    //-------------------------------------------------------------------------------
+    // Template
+    //-------------------------------------------------------------------------------
+
+    template:   '<div id="{{id}}" class="btn-group {{buttonGroupClasses}}">' +
+        '</div>',
+
+
+    //-------------------------------------------------------------------------------
+    // MustacheView Implementation
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @return {Object}
+     */
+    generateTemplateData: function() {
+        var data = this._super();
+
+        data.buttonGroupClasses = "";
+        switch (this.attributes.align) {
+            case "right":
+                data.buttonGroupClasses += "pull-right";
+                break;
+        }
+
+        return data;
+    }
+});
+
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export("airbug.ButtonGroupView", ButtonGroupView);
