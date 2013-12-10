1a855b52685c588cbbcd1d2b8f8de6a4f47a717b
Sung Choi <m.sung.choi@gmail.com>
2013-12-10 01:08:00 -0800

    Adds ShareRoomButton file and class. This button will replace the current function of the AddRoomMemberButton which is being inappropriately used to share the room url.

---
+++ projects/airbugclient/js/src/containers/pages/roompage/shareroom/ShareRoomButton.js
@@ -0,0 +1,164 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbug')
+
+//@Export('ShareRoomButtonContainer')
+
+//@Require('Class')
+//@Require('airbug.ButtonContainer')
+//@Require('airbug.ButtonView')
+//@Require('airbug.ButtonViewEvent')
+//@Require('airbug.CommandModule')
+//@Require('airbug.IconView')
+//@Require('bugioc.AutowiredAnnotation')
+//@Require('bugioc.PropertyAnnotation')
+//@Require('bugmeta.BugMeta')
+//@Require('carapace.ViewBuilder')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// BugPack
+//-------------------------------------------------------------------------------
+
+var Class                   = bugpack.require('Class');
+var ButtonContainer         = bugpack.require('airbug.ButtonContainer');
+var ButtonView              = bugpack.require('airbug.ButtonView');
+var ButtonViewEvent         = bugpack.require('airbug.ButtonViewEvent');
+var CommandModule           = bugpack.require('airbug.CommandModule');
+var IconView                = bugpack.require('airbug.IconView');
+var AutowiredAnnotation     = bugpack.require('bugioc.AutowiredAnnotation');
+var PropertyAnnotation      = bugpack.require('bugioc.PropertyAnnotation');
+var BugMeta                 = bugpack.require('bugmeta.BugMeta');
+var ViewBuilder             = bugpack.require('carapace.ViewBuilder');
+
+
+//-------------------------------------------------------------------------------
+// Simplify References
+//-------------------------------------------------------------------------------
+
+var autowired   = AutowiredAnnotation.autowired;
+var bugmeta     = BugMeta.context();
+var CommandType = CommandModule.CommandType;
+var property    = PropertyAnnotation.property;
+var view        = ViewBuilder.view;
+
+
+//-------------------------------------------------------------------------------
+// Declare Class
+//-------------------------------------------------------------------------------
+
+var ShareRoomButtonContainer = Class.extend(ButtonContainer, {
+
+    //-------------------------------------------------------------------------------
+    // Constructor
+    //-------------------------------------------------------------------------------
+
+    _constructor: function(roomModel) {
+
+        this._super("ShareRoomButton");
+
+
+        //-------------------------------------------------------------------------------
+        // Declare Variables
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @private
+         * @type {RoomModel}
+         */
+        this.roomModel          = roomModel;
+
+
+        // Modules
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @private
+         * @type {CommandModule}
+         */
+        this.commandModule      = null;
+
+
+        // Views
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @private
+         * @type {ButtonView}
+         */
+        this.buttonView         = null;
+    },
+
+
+    //-------------------------------------------------------------------------------
+    // CarapaceContainer Extensions
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @protected
+     */
+    createContainer: function() {
+        this._super();
+
+        // Create Views
+        //-------------------------------------------------------------------------------
+
+        this.buttonView =
+            view(ButtonView)
+                .id("shareRoomButton")
+                .attributes({type: "link"})
+                .children([
+                    view(IconView)
+                        .attributes({type: IconView.Type.SHARE_ALT})
+                        .appendTo('*[id|="button"]')
+                ])
+                .build();
+
+
+        // Wire Up Views
+        //-------------------------------------------------------------------------------
+
+        this.setViewTop(this.buttonView);
+    },
+
+    /**
+     * @protected
+     */
+    createContainerChildren: function() {
+        this._super();
+    },
+
+    /**
+     * @protected
+     */
+    initializeContainer: function() {
+        this._super();
+    }
+});
+
+
+//-------------------------------------------------------------------------------
+// BugMeta
+//-------------------------------------------------------------------------------
+
+bugmeta.annotate(ShareRoomButtonContainer).with(
+    autowired().properties([
+        property("commandModule").ref("commandModule")
+    ])
+);
+
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export("airbug.ShareRoomButtonContainer", ShareRoomButtonContainer);
