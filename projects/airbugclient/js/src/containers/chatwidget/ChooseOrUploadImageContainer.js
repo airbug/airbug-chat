232ec9efe27ac68ece0268b91dc8cc4f56ede152
Sung Choi <m.sung.choi@gmail.com>
2013-12-09 17:21:58 -0800

    Interim Commit. Adds file for and outlines ChooseOrUploadImageContainer class.

---
+++ projects/airbugclient/js/src/containers/chatwidget/ChooseOrUploadImageContainer.js
@@ -0,0 +1,194 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbug')
+
+//@Export('ChooseOrUploadImageContainer')
+
+//@Require('Class')
+//@Require('airbug.BoxView')
+//@Require('airbug.CommandModule')
+//@Require('bugioc.AutowiredAnnotation')
+//@Require('bugioc.PropertyAnnotation')
+//@Require('bugmeta.BugMeta')
+//@Require('carapace.CarapaceContainer')
+//@Require('carapace.ViewBuilder')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// BugPack
+//-------------------------------------------------------------------------------
+
+var Class                               = bugpack.require('Class');
+var BoxView                             = bugpack.require('airbug.BoxView');
+var CommandModule                       = bugpack.require('airbug.CommandModule');
+var AutowiredAnnotation                 = bugpack.require('bugioc.AutowiredAnnotation');
+var PropertyAnnotation                  = bugpack.require('bugioc.PropertyAnnotation');
+var BugMeta                             = bugpack.require('bugmeta.BugMeta');
+var CarapaceContainer                   = bugpack.require('carapace.CarapaceContainer');
+var ViewBuilder                         = bugpack.require('carapace.ViewBuilder');
+
+
+//-------------------------------------------------------------------------------
+// Simplify References
+//-------------------------------------------------------------------------------
+
+var CommandType = CommandModule.CommandType;
+var view        = ViewBuilder.view;
+
+
+//-------------------------------------------------------------------------------
+// Declare Class
+//-------------------------------------------------------------------------------
+
+var ChooseOrUploadImageContainer = Class.extend(CarapaceContainer, {
+
+    //-------------------------------------------------------------------------------
+    // Constructor
+    //-------------------------------------------------------------------------------
+
+    _constructor: function() {
+
+        this._super();
+
+
+        //-------------------------------------------------------------------------------
+        // Declare Variables
+        //-------------------------------------------------------------------------------
+
+        // Models
+        //-------------------------------------------------------------------------------
+
+
+        // Modules
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @private
+         * @type {CommandModule}
+         */
+        this.commandModule              = null;
+
+        // Views
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @private
+         * @type {BoxView}
+         */
+        this.boxView                    = null;
+
+
+        // Containers
+        //-------------------------------------------------------------------------------
+
+
+    },
+
+
+    //-------------------------------------------------------------------------------
+    // CarapaceController Implementation
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @protected
+     * @param {Array<*>} routerArgs
+     */
+    activateContainer: function(routerArgs) {
+        this._super(routerArgs);
+
+    },
+
+    /**
+     * @protected
+     */
+    createContainer: function() {
+        this._super();
+
+
+        // Create Models
+        //-------------------------------------------------------------------------------
+
+
+        // Create Views
+        //-------------------------------------------------------------------------------
+
+        this.boxView =
+            view(BoxView)
+                .build();
+
+        // Wire Up Views
+        //-------------------------------------------------------------------------------
+
+        this.setViewTop(this.boxView);
+    },
+
+    createContainerChildren: function() {
+        this._super();
+    },
+
+    /**
+     * @protected
+     */
+    initializeContainer: function() {
+        this._super();
+        this.initializeCommandSubscriptions();
+    },
+
+    /**
+     * @protected
+     */
+    deinitializeContainer: function() {
+        this._super();
+        this.deinitializeCommandSubscriptions();
+    },
+
+    //-------------------------------------------------------------------------------
+    // Event Listeners
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @private
+     */
+    initializeCommandSubscriptions: function() {
+
+    },
+
+    /**
+     * @private
+     */
+    deinitializeCommandSubscriptions: function() {
+
+    }
+
+    //-------------------------------------------------------------------------------
+    // Event Handlers
+    //-------------------------------------------------------------------------------
+
+});
+
+
+//-------------------------------------------------------------------------------
+// BugMeta
+//-------------------------------------------------------------------------------
+
+bugmeta.annotate(ChooseOrUploadImageContainer).with(
+    autowired().properties([
+        property("commandModule").ref("commandModule")
+    ])
+);
+
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export("airbug.ChooseOrUploadImageContainer", ChooseOrUploadImageContainer);
