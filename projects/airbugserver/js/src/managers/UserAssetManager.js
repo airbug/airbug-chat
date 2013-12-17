82f6ff131b1a056e8b8a4f2b2ca8837f5eb28f40
Dustin Kanske <dustin@kanske.com>
2013-12-16 15:41:54 -1000

    Add UserAssetManager and start of tests

---
+++ projects/airbugserver/js/src/managers/UserAssetManager.js
@@ -0,0 +1,186 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbugserver')
+
+//@Export('UserAssetManager')
+//@Autoload
+
+//@Require('Class')
+//@Require('Map')
+//@Require('TypeUtil')
+//@Require('airbugserver.UserAsset')
+//@Require('bugentity.EntityManager')
+//@Require('bugentity.EntityManagerAnnotation')
+//@Require('bugioc.ArgAnnotation')
+//@Require('bugmeta.BugMeta')
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack                     = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// Bugpack Modules
+//-------------------------------------------------------------------------------
+
+var Class                       = bugpack.require('Class');
+var Map                         = bugpack.require('Map');
+var TypeUtil                    = bugpack.require('TypeUtil');
+var UserAsset                   = bugpack.require('airbugserver.UserAsset');
+var EntityManager               = bugpack.require('bugentity.EntityManager');
+var EntityManagerAnnotation     = bugpack.require('bugentity.EntityManagerAnnotation');
+var ArgAnnotation               = bugpack.require('bugioc.ArgAnnotation');
+var BugMeta                     = bugpack.require('bugmeta.BugMeta');
+
+
+//-------------------------------------------------------------------------------
+// Simplify References
+//-------------------------------------------------------------------------------
+
+var arg                         = ArgAnnotation.arg;
+var bugmeta                     = BugMeta.context();
+var entityManager               = EntityManagerAnnotation.entityManager;
+
+
+//-------------------------------------------------------------------------------
+// Declare Class
+//-------------------------------------------------------------------------------
+
+var UserAssetManager = Class.extend(EntityManager, {
+
+
+    //-------------------------------------------------------------------------------
+    // Public Instance Methods
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @param {UserAsset} userAsset
+     * @param {(Array.<string> | function(Throwable, UserAsset))} dependencies
+     * @param {function(Throwable, UserAsset)} callback
+     */
+    createUserAsset: function(userAsset, dependencies, callback) {
+        if (TypeUtil.isFunction(dependencies)) {
+            callback        = dependencies;
+            dependencies    = [];
+        }
+        var options         = {};
+        this.create(userAsset, options, dependencies, callback);
+    },
+
+    /**
+     * @param {UserAsset} userAsset
+     * @param {function(Throwable)} callback
+     */
+    deleteUserAsset: function(userAsset, callback) {
+        this.delete(userAsset, callback);
+    },
+
+    /**
+     * @param {{
+     *      assetId: string,
+     *      createdAt: Date,
+     *      updatedAt: Date,
+     *      userId: string
+     * }} data
+     * @return {UserAsset}
+     */
+    generateUserAsset: function(data) {
+        var userAsset = new UserAsset(data);
+        console.log("UserAssetManager#generateUserAsset userAsset = ", userAsset);
+        this.generate(userAsset);
+        return userAsset;
+    },
+
+    /**
+     * @param {UserAsset} userAsset
+     * @param {Array.<string>} properties
+     * @param {function(Throwable, UserAsset)} callback
+     */
+    populateUserAsset: function(userAsset, properties, callback) {
+        var options = {
+            asset: {
+                idGetter:   userAsset.getAssetId,
+                idSetter:   userAsset.setAssetId,
+                getter:     userAsset.getAsset,
+                setter:     userAsset.setAsset
+            },
+            user: {
+                idGetter:   userAsset.getUserId,
+                idSetter:   userAsset.setUserId,
+                getter:     userAsset.getUser,
+                setter:     userAsset.setUser
+            }
+        };
+        this.populate(userAsset, options, properties, callback);
+    },
+
+    /**
+     * @param {Array.<string>} userAssetId
+     * @param {function(Throwable, UserAsset)} callback
+     */
+    retrieveUserAsset: function(userAssetId, callback) {
+        this.retrieve(userAssetId, callback);
+    },
+
+    /**
+     * @param {string} userAssetIds
+     * @param {function(Throwable, Map.<string, UserAsset>)} callback
+     */
+    retrieveUserAssets: function(userAssetIds, callback) {
+        this.retrieveEach(userAssetIds, callback);
+    },
+
+    /**
+     * @param {string} userId
+     * @param {function(Throwable, Map.<string, UserAsset>)} callback
+     */
+    retrieveUserAssetsByUserId: function(userId, callback) {
+        var _this = this;
+        this.dataStore.find({userId: userId}).lean(true).exec(function(throwable, dbObjects) {
+            if (!throwable) {
+                var newMap = Map();
+                dbObjects.forEach(function(dbObject) {
+                    var userAsset = _this.convertDbObjectToEntity(dbObject);
+                    userAsset.commitDelta();
+                    newMap.put(userAsset.getId(), userAsset);
+                });
+                callback(undefined, newMap);
+            } else {
+                callback(throwable, undefined);
+            }
+        });
+    },
+
+    /**
+     * @param {UserAsset} userAsset
+     * @param {function(Throwable, UserAsset)} callback
+     */
+    updateUserAsset: function(userAsset, callback) {
+        this.update(userAsset, callback);
+    }
+});
+
+//-------------------------------------------------------------------------------
+// BugMeta
+//-------------------------------------------------------------------------------
+
+bugmeta.annotate(UserAssetManager).with(
+    entityManager("userAssetManager")
+        .ofType("UserAsset")
+        .args([
+            arg().ref("entityManagerStore"),
+            arg().ref("schemaManager"),
+            arg().ref("mongoDataStore")
+        ])
+);
+
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export('airbugserver.UserAssetManager', UserAssetManager);
---
+++ projects/airbugserver/js/test/managers/UserAssetManagerTests.js
@@ -0,0 +1,194 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@TestFile
+
+//@Require('Set')
+//@Require('airbugserver.Asset')
+//@Require('airbugserver.AssetManager')
+//@Require('airbugserver.User')
+//@Require('airbugserver.UserManager')
+//@Require('airbugserver.UserAsset')
+//@Require('airbugserver.UserAssetManager')
+//@Require('bugentity.EntityManagerStore')
+//@Require('bugentity.SchemaManager')
+//@Require('bugflow.BugFlow')
+//@Require('bugmeta.BugMeta')
+//@Require('bugunit-annotate.TestAnnotation')
+//@Require('mongo.DummyMongoDataStore')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack                 = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// BugPack
+//-------------------------------------------------------------------------------
+
+var Set                     = bugpack.require('Set');
+var Asset                   = bugpack.require('airbugserver.Asset');
+var AssetManager            = bugpack.require('airbugserver.AssetManager');
+var User                    = bugpack.require('airbugserver.User');
+var UserManager             = bugpack.require('airbugserver.UserManager');
+var UserAsset               = bugpack.require('airbugserver.UserAsset');
+var UserAssetManager        = bugpack.require('airbugserver.UserAssetManager');
+var EntityManagerStore      = bugpack.require('bugentity.EntityManagerStore');
+var SchemaManager           = bugpack.require('bugentity.SchemaManager');
+var BugFlow                 = bugpack.require('bugflow.BugFlow');
+var BugMeta                 = bugpack.require('bugmeta.BugMeta');
+var TestAnnotation          = bugpack.require('bugunit-annotate.TestAnnotation');
+var DummyMongoDataStore     = bugpack.require('mongo.DummyMongoDataStore');
+
+
+//-------------------------------------------------------------------------------
+// Simplify References
+//-------------------------------------------------------------------------------
+
+var bugmeta                 = BugMeta.context();
+var test                    = TestAnnotation.test;
+var $series                 = BugFlow.$series;
+var $task                   = BugFlow.$task;
+
+
+//-------------------------------------------------------------------------------
+// Declare Setup Objects
+//-------------------------------------------------------------------------------
+
+var setupUserAssetManager = function(setupObject) {
+    setupObject.testUser               = new User({});
+    setupObject.testAsset              = new Asset({});
+    setupObject.entityManagerStore     = new EntityManagerStore();
+    setupObject.mongoDataStore         = new DummyMongoDataStore();
+    setupObject.schemaManager          = new SchemaManager();
+    setupObject.assetManager           = new AssetManager(setupObject.entityManagerStore, setupObject.schemaManager, setupObject.mongoDataStore);
+    setupObject.assetManager.setEntityType("Asset");
+    setupObject.userManager            = new UserManager(setupObject.entityManagerStore, setupObject.schemaManager, setupObject.mongoDataStore);
+    setupObject.userManager.setEntityType("User");
+    setupObject.userAssetManager       = new UserAssetManager(setupObject.entityManagerStore, setupObject.schemaManager, setupObject.mongoDataStore);
+    setupObject.userAssetManager.setEntityType("UserAsset");
+};
+
+var initializeManagers = function(setupObject, test) {
+    var _this = setupObject;
+    $series([
+        $task(function(flow) {
+            _this.schemaManager.initializeModule(function(throwable) {
+                flow.complete(throwable);
+            });
+        }),
+        $task(function(flow) {
+            _this.userAssetManager.initializeModule(function(throwable) {
+                flow.complete(throwable);
+            });
+        }),
+        $task(function(flow) {
+            _this.assetManager.initializeModule(function(throwable) {
+                flow.complete(throwable);
+            });
+        }),
+        $task(function(flow) {
+            _this.userManager.initializeModule(function(throwable) {
+                flow.complete(throwable);
+            });
+        })
+    ]).execute(function(throwable) {
+        if (throwable) {
+            test.error(throwable);
+        }
+    });
+};
+
+var createEntities = function(setupObject, test) {
+    var _this = setupObject;
+    $series([
+        $task(function(flow) {
+            _this.userManager.createUser(_this.testUser, function(throwable, user) {
+                if (!throwable) {
+                    _this.testUserId = user.getId();
+                    flow.complete();
+                } else {
+                    flow.complete(throwable);
+                }
+            });
+        }),
+        $task(function(flow) {
+            _this.assetManager.createAsset(_this.testAsset, function(throwable, asset) {
+                if (!throwable) {
+                    _this.testAssetId = asset.getId();
+                    flow.complete();
+                } else {
+                    flow.complete(throwable);
+                }
+            });
+        }),
+        $task(function(flow) {
+            _this.testUserAsset = _this.userAssetManager.generateUserAsset({
+                assetId: _this.testAssetId,
+                userId: _this.testUserId
+            });
+            flow.complete();
+        })
+    ]).execute(function(throwable) {
+        if (throwable) {
+            test.error(throwable);
+        }
+    });
+};
+
+
+//-------------------------------------------------------------------------------
+// Declare Tests
+//-------------------------------------------------------------------------------
+
+var userAssetManagerCreateUserAssetTest = {
+
+    async: true,
+
+    //-------------------------------------------------------------------------------
+    // Setup Test
+    //-------------------------------------------------------------------------------
+
+    setup: function(test) {
+        setupUserAssetManager(this);
+    },
+
+
+    //-------------------------------------------------------------------------------
+    // Run Test
+    //-------------------------------------------------------------------------------
+
+    test: function(test) {
+        var _this = this;
+        initializeManagers(this, test);
+        createEntities(this, test);
+        $series([
+            $task(function(flow) {
+                _this.userAssetManager.createUserAsset(_this.testUserAsset, function(throwable, userAsset) {
+                    if (!throwable) {
+                        test.assertEqual(_this.testUserAsset, userAsset,
+                            "Assert userAsset returned is the same userAsset sent in");
+                        var id = userAsset.getId();
+                        test.assertTrue(!!id,
+                            "Assert create UserAsset has an id. id = " + id);
+                    }
+                    flow.complete(throwable);
+                });
+            })
+        ]).execute(function(throwable) {
+            if (!throwable) {
+                test.complete();
+            } else {
+                test.error(throwable);
+            }
+        });
+    }
+};
+
+bugmeta.annotate(userAssetManagerCreateUserAssetTest).with(
+    test().name("UserAssetManager - #createUserAsset Test")
+);
