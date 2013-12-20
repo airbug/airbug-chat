597bf9fd517a2b13443bdc01ca6ef36e54113ac1
Dustin Kanske <dustin@kanske.com>
2013-12-20 02:56:52 -0800

    add stubs for user asset service

---
+++ projects/airbugserver/js/src/services/UserAssetService.js
@@ -0,0 +1,128 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbugserver')
+
+//@Export('UserAssetService')
+
+//@Require('Class')
+//@Require('Exception')
+//@Require('Obj')
+//@Require('Set')
+//@Require('airbugserver.UserAsset')
+//@Require('bugflow.BugFlow')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack             = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// Bugpack Modules
+//-------------------------------------------------------------------------------
+
+var Class               = bugpack.require('Class');
+var Exception           = bugpack.require('Exception');
+var Obj                 = bugpack.require('Obj');
+var Set                 = bugpack.require('Set');
+var UserAsset           = bugpack.require('airbugserver.UserAsset');
+var BugFlow             = bugpack.require('bugflow.BugFlow');
+
+
+//-------------------------------------------------------------------------------
+// Simplify References
+//-------------------------------------------------------------------------------
+
+var $parallel           = BugFlow.$parallel;
+var $series             = BugFlow.$series;
+var $task               = BugFlow.$task;
+var $iterableParallel   = BugFlow.$iterableParallel;
+
+
+//-------------------------------------------------------------------------------
+// Declare Class
+//-------------------------------------------------------------------------------
+
+var RoomService = Class.extend(Obj, {
+
+    //-------------------------------------------------------------------------------
+    // Constructor
+    //-------------------------------------------------------------------------------
+
+    // TODO: wire this up in AirbugServerConfig
+    _constructor: function(userAssetManager) {
+
+        this._super();
+
+
+        //-------------------------------------------------------------------------------
+        // Declare Variables
+        //-------------------------------------------------------------------------------
+
+        /**
+         * @private
+         * @type {Logger}
+         */
+        this.logger                 = null;
+
+        /**
+         * @private
+         * @type {UserAssetManager}
+         */
+        this.userAssetManager            = userAssetManager;
+    },
+
+
+    //-------------------------------------------------------------------------------
+    // Service Methods
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @param {RequestContext} requestContext
+     * @param {UserAsset} userAssetObject
+     * @param {Function(Throwable, UserAsset)} callback
+     */
+    createUserAsset: function(requestContext, userAssetObject, callback) {
+
+    },
+
+    /**
+     * @param {RequestContext} requestContext
+     * @param {string} userAssetId
+     * @param {string} userAssetName
+     * @param {function(Throwable, UserAsset)} callback
+     */
+    renameUserAsset: function(requestContext, userAssetId, userAssetName, callback) {
+
+    },
+
+    /**
+     * @param {RequestContext} requestContext
+     * @param {string} userAssetId
+     * @param {Function(Throwable, UserAsset)} callback
+     */
+    retrieveUserAsset: function(requestContext, userAssetId, callback) {
+
+    },
+
+    /**
+     * @param {RequestContext} requestContext
+     * @param {Array.<string>} userAssetIds
+     * @param {function(Throwable, Map.<string, Room>)} callback
+     */
+    retrieveUserAssets: function(requestContext, userAssetIds, callback) {
+
+    }
+});
+
+
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export('airbugserver.UserAssetService', UserAssetService);
