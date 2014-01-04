03187c68730733c93a406d51d012191311ea5144
Sung Choi <m.sung.choi@gmail.com>
2014-01-03 16:41:09 -0800

    New. Adds OverlayView and its css.

---
+++ projects/airbugclient/js/src/views/common/overlays/OverlayView.js
@@ -0,0 +1,140 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbug')
+
+//@Export('OverlayView')
+
+//@Require('Class')
+//@Require('airbug.ButtonViewEvent')
+//@Require('airbug.MustacheView')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// BugPack
+//-------------------------------------------------------------------------------
+
+var Class           = bugpack.require('Class');
+var MustacheView    = bugpack.require('airbug.MustacheView');
+
+
+//-------------------------------------------------------------------------------
+// Declare Class
+//-------------------------------------------------------------------------------
+
+var OverlayView = Class.extend(MustacheView, {
+
+    //-------------------------------------------------------------------------------
+    // Template
+    //-------------------------------------------------------------------------------
+
+    template:   '<div id="{{id}}" class="{{overlayWrapperClasses}}">' +
+            '<div class="overlay-background">' +
+            '</div>' +
+            '<div class="{{overlayClasses}}">' +
+            '</div>' +
+        '</div>',
+
+
+    //-------------------------------------------------------------------------------
+    // CarapaceView Extensions
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @protected
+     */
+    deinitializeView: function() {
+        this._super();
+        this.findElement('.overlay-background').off();
+    },
+
+    /**
+     * @protected
+     */
+    initializeView: function() {
+        this._super();
+        var _this = this;
+        this.findElement('.overlay-background').on('click', function(event) {
+            _this.handleBackgroundClick(event);
+        });
+    },
+
+    //-------------------------------------------------------------------------------
+    // MustacheView Extensions
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @return {Object}
+     */
+    generateTemplateData: function() {
+        var data    = this._super();
+        data.id     = this.getId() || "overlay-" + this.getCid();
+
+        if(this.attributes.classes){
+            data.overlayWrapperClasses = this.attributes.classes + " overlay-wrapper";
+        } else {
+            data.overlayWrapperClasses = "overlay-wrapper";
+        }
+        data.overlayClasses = "overlay";
+        switch (this.attributes.type) {
+            case OverlayView.Type.PAGE:
+                data.overlayWrapperClasses += " page-overlay";
+                break;
+            case OverlayView.Type.APPLICATION:
+                data.overlayWrapperClasses += " application-overlay";
+                break;
+        }
+        switch (this.attributes.size) {
+            case OverlayView.Size.ONE_HALF:
+                data.overlayClasses += " one-half-overlay";
+                break;
+            case OverlayView.Size.ONE_THIRD:
+                data.overlayClasses += " one-third-overlay";
+                break;
+            case OverlayView.Size.FULLSCREEN:
+                data.overlayClasses += " fullscreen-overlay";
+                break;
+        }
+        return data;
+    },
+
+    //-------------------------------------------------------------------------------
+    // View Event Handlers
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {jQuery.Event} event
+     */
+    handleBackgroundClick: function(event) {
+        event.preventDefault();
+        this.dispatchEvent(new ButtonViewEvent(ButtonViewEvent.EventType.CLICKED));
+    }
+});
+
+OverlayView.Type = {
+    APPLICATION: "application-overlay",
+    PAGE: "page-overlay"
+};
+
+OverlayView.Size = {
+    ONE_QUARTER: "one-quarter-overlay",
+    ONE_THIRD:   "one-third-overlay",
+    ONE_HALF:    "one-half-overlay",
+    FULLSCREEN:  "fullscreen-overlay"
+};
+
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export("airbug.OverlayView", OverlayView);
