airbug/airbug-chat

commit f8debe0849ab561ab0771c5843a9246239e844e4

Refactor. Refactors share room button and add roommember button to use the new overlay view.

projects/airbugclient/js/src/containers/pages/roompage/RoomPageContainer.js
@@ -8,12 +8,15 @@
 
 //@Require('Class')
 //@Require('Exception')
+//@Require('airbug.ButtonViewEvent')
+//@Require('airbug.CommandModule')
 //@Require('airbug.HomeButtonContainer')
 //@Require('airbug.LogoutButtonContainer')
 //@Require('airbug.PageContainer')
 //@Require('airbug.RoomChatBoxContainer')
 //@Require('airbug.RoomListPanelContainer')
 //@Require('airbug.RoomModel')
+//@Require('airbug.ShareRoomContainer')
 //@Require('bugmeta.BugMeta')
 //@Require('bugioc.AutowiredAnnotation')
 //@Require('bugioc.PropertyAnnotation')
@@ -33,12 +36,15 @@ var bugpack                     = require('bugpack').context();
 
 var Class                       = bugpack.require('Class');
 var Exception                   = bugpack.require('Exception');
+var ButtonViewEvent             = bugpack.require('airbug.ButtonViewEvent')
+var CommandModule               = bugpack.require('airbug.CommandModule');
 var HomeButtonContainer         = bugpack.require('airbug.HomeButtonContainer');
 var LogoutButtonContainer       = bugpack.require('airbug.LogoutButtonContainer');
 var PageContainer               = bugpack.require('airbug.PageContainer');
 var RoomChatBoxContainer        = bugpack.require('airbug.RoomChatBoxContainer');
 var RoomListPanelContainer      = bugpack.require('airbug.RoomListPanelContainer');
 var RoomModel                   = bugpack.require('airbug.RoomModel');
+var ShareRoomContainer          = bugpack.require('airbug.ShareRoomContainer');
 var BugMeta                     = bugpack.require('bugmeta.BugMeta');
 var AutowiredAnnotation         = bugpack.require('bugioc.AutowiredAnnotation');
 var PropertyAnnotation          = bugpack.require('bugioc.PropertyAnnotation');
@@ -49,8 +55,9 @@ var ViewBuilder                 = bugpack.require('carapace.ViewBuilder');
 // Simplify References
 //-------------------------------------------------------------------------------
 
-var bugmeta                     = BugMeta.context();
 var autowired                   = AutowiredAnnotation.autowired;
+var bugmeta                     = BugMeta.context();
+var CommandType                 = CommandModule.CommandType;
 var property                    = PropertyAnnotation.property;
 var view                        = ViewBuilder.view;
 
@@ -113,6 +120,12 @@ var RoomPageContainer = Class.extend(PageContainer, {
          */
         this.roomsHamburgerButtonContainer          = null;
 
+        /**
+         * @private
+         * @type {ShareRoomContainer}
+         */
+        this.shareRoomContainer                     = null;
+
 
         // Models
         //-------------------------------------------------------------------------------
@@ -130,6 +143,11 @@ var RoomPageContainer = Class.extend(PageContainer, {
 
         // Modules
         //-------------------------------------------------------------------------------
+
+        /**
+         * @type {CommandModule}
+         */
+        this.commandModule                          = null;
 
         /**
          * @type {NavigationModule}
@@ -182,12 +200,59 @@ var RoomPageContainer = Class.extend(PageContainer, {
         this.roomChatBoxContainer                   = new RoomChatBoxContainer(this.roomModel);
         this.roomListPanelContainer                 = new RoomListPanelContainer();
 
+        this.shareRoomContainer                     = new ShareRoomContainer(this.roomModel);
+        this.addContainerChild(this.shareRoomContainer,             "#page-row-container");
+
+
         this.addContainerChild(this.logoutButtonContainer,          "#header-right");
         this.addContainerChild(this.homeButtonContainer,            "#header-left");
         this.addContainerChild(this.roomListPanelContainer,         ".column1of4");
         this.addContainerChild(this.roomChatBoxContainer,           ".column2of4");
     },
 
+    initializeContainer: function() {
+        this._super();
+        this.initializeEventListeners();
+        this.initializeCommandSubscriptions();
+    },
+
+    deinitializeContainer: function() {
+        this._super();
+        this.deinitializeEventListeners();
+        this.deinitializeCommandSubscriptions();
+    },
+
+    initializeEventListeners: function() {
+        var overlayView  = this.shareRoomContainer.getViewTop();
+        overlayView.addEventListener(ButtonViewEvent.EventType.CLICKED, this.hearOverlayBackgroundClickedEvent, this);
+    },
+
+    deinitializeEventListeners: function() {
+        var overlayView  = this.shareRoomContainer.getViewTop();
+        overlayView.removeEventListener(ButtonViewEvent.EventType.CLICKED, this.hearOverlayBackgroundClickedEvent, this);
+    },
+
+    initializeCommandSubscriptions: function() {
+        this.commandModule.subscribe(CommandType.DISPLAY.SHARE_ROOM_OVERLAY, this.handleDisplayShareRoomOverlayCommand, this);
+        this.commandModule.subscribe(CommandType.HIDE.SHARE_ROOM_OVERLAY, this.handleHideShareRoomOverlayCommand, this);
+    },
+
+    deinitializeCommandSubscriptions: function() {
+        this.commandModule.unsubscribe(CommandType.DISPLAY.SHARE_ROOM_OVERLAY, this.handleDisplayShareRoomOverlayCommand, this);
+        this.commandModule.unsubscribe(CommandType.HIDE.SHARE_ROOM_OVERLAY, this.handleHideShareRoomOverlayCommand, this);
+    },
+
+    hearOverlayBackgroundClickedEvent: function() {
+        this.viewTop.$el.find(".share-room-overlay").hide();
+    },
+
+    handleDisplayShareRoomOverlayCommand: function() {
+        this.viewTop.$el.find(".share-room-overlay").show();
+    },
+
+    handleHideShareRoomOverlayCommand: function() {
+        this.viewTop.$el.find(".share-room-overlay").hide();
+    },
 
     //-------------------------------------------------------------------------------
     // Protected Class Methods
@@ -240,6 +305,7 @@ var RoomPageContainer = Class.extend(PageContainer, {
 
 bugmeta.annotate(RoomPageContainer).with(
     autowired().properties([
+        property("commandModule").ref("commandModule"),
         property("navigationModule").ref("navigationModule"),
         property("roomManagerModule").ref("roomManagerModule")
     ])

projects/airbugclient/js/src/containers/pages/roompage/addroommember/AddRoomMemberButtonContainer.js
@@ -7,12 +7,12 @@
 //@Export('AddRoomMemberButtonContainer')
 
 //@Require('Class')
-//@Require('airbug.AddRoomMemberContainer')
 //@Require('airbug.ButtonContainer')
 //@Require('airbug.ButtonView')
 //@Require('airbug.ButtonViewEvent')
+//@Require('airbug.CommandModule')
 //@Require('airbug.IconView')
-//@Require('airbug.NakedButtonDropdownView')
+//@Require('airbug.NakedButtonView')
 //@Require('bugioc.AutowiredAnnotation')
 //@Require('bugioc.PropertyAnnotation')
 //@Require('bugmeta.BugMeta')
@@ -31,12 +31,12 @@ var bugpack = require('bugpack').context();
 //-------------------------------------------------------------------------------
 
 var Class                   = bugpack.require('Class');
-var AddRoomMemberContainer  = bugpack.require('airbug.AddRoomMemberContainer');
 var ButtonContainer         = bugpack.require('airbug.ButtonContainer');
 var ButtonView              = bugpack.require('airbug.ButtonView');
 var ButtonViewEvent         = bugpack.require('airbug.ButtonViewEvent');
+var CommandModule           = bugpack.require('airbug.CommandModule');
 var IconView                = bugpack.require('airbug.IconView');
-var NakedButtonDropdownView = bugpack.require('airbug.NakedButtonDropdownView');
+var NakedButtonView         = bugpack.require('airbug.NakedButtonView');
 var AutowiredAnnotation     = bugpack.require('bugioc.AutowiredAnnotation');
 var PropertyAnnotation      = bugpack.require('bugioc.PropertyAnnotation');
 var BugMeta                 = bugpack.require('bugmeta.BugMeta');
@@ -49,6 +49,7 @@ var ViewBuilder             = bugpack.require('carapace.ViewBuilder');
 
 var autowired   = AutowiredAnnotation.autowired;
 var bugmeta     = BugMeta.context();
+var CommandType = CommandModule.CommandType;
 var property    = PropertyAnnotation.property;
 var view        = ViewBuilder.view;
 
@@ -84,9 +85,9 @@ var AddRoomMemberButtonContainer = Class.extend(ButtonContainer, {
 
         /**
          * @private
-         * @type {NavigationModule}
+         * @type {CommandModule}
          */
-        this.navigationModule   = null;
+        this.commandModule      = null;
 
 
         // Views
@@ -114,14 +115,14 @@ var AddRoomMemberButtonContainer = Class.extend(ButtonContainer, {
         //-------------------------------------------------------------------------------
 
         this.buttonView =
-            view(NakedButtonDropdownView)
+            view(NakedButtonView)
                 .attributes({
                     type: ButtonView.Type.LINK
                 })
                 .children([
                     view(IconView)
                         .attributes({
-                            type: IconView.Type.PLUS,
+                            type: IconView.Type.PLUS
                         })
                         .appendTo('button[id|="button"]')
                 ])
@@ -139,8 +140,6 @@ var AddRoomMemberButtonContainer = Class.extend(ButtonContainer, {
      */
     createContainerChildren: function() {
         this._super();
-        this.addRoomMemberContainer        = new AddRoomMemberContainer(this.roomModel);
-        this.addContainerChild(this.addRoomMemberContainer,   "#dropdown-list-" + this.buttonView.getCid());
     },
 
     /**
@@ -148,7 +147,16 @@ var AddRoomMemberButtonContainer = Class.extend(ButtonContainer, {
      */
     initializeContainer: function() {
         this._super();
-        this.buttonView.$el.find("li:first-child").addClass("add-roommember-container");
+        this.buttonView.addEventListener(ButtonViewEvent.EventType.CLICKED, this.hearButtonClickedEvent, this);
+    },
+
+    deinitializeContainer: function() {
+        this.super();
+        this.buttonView.removeEventListener(ButtonViewEvent.EventType.CLICKED, this.hearButtonClickedEvent, this);
+    },
+
+    hearButtonClickedEvent: function(event) {
+        this.commandModule.relayCommand(CommandType.DISPLAY.SHARE_ROOM_OVERLAY, {});
     }
 });
 
@@ -159,7 +167,7 @@ var AddRoomMemberButtonContainer = Class.extend(ButtonContainer, {
 
 bugmeta.annotate(AddRoomMemberButtonContainer).with(
     autowired().properties([
-        property("navigationModule").ref("navigationModule")
+        property("commandModule").ref("commandModule")
     ])
 );
 

projects/airbugclient/js/src/containers/pages/roompage/addroommember/AddRoomMemberContainer.js
@@ -1,312 +1,0 @@
-//-------------------------------------------------------------------------------
-// Annotations
-//-------------------------------------------------------------------------------
-
-//@Package('airbug')
-
-//@Export('AddRoomMemberContainer')
-
-//@Require('Class')
-//@Require('airbug.BoxView')
-//@Require('airbug.ButtonView')
-//@Require('airbug.ButtonViewEvent')
-//@Require('airbug.CopyToClipboardButtonView')
-//@Require('airbug.DropdownItemView')
-//@Require('airbug.DropdownViewEvent')
-//@Require('airbug.FauxTextAreaView')
-//@Require('airbug.IconView')
-//@Require('airbug.ParagraphView')
-//@Require('airbug.TextView')
-//@Require('bugioc.AutowiredAnnotation')
-//@Require('bugioc.PropertyAnnotation')
-//@Require('bugmeta.BugMeta')
-//@Require('carapace.CarapaceContainer')
-//@Require('carapace.ViewBuilder')
-//@Require('zeroclipboard.ZeroClipboard')
-
-
-//-------------------------------------------------------------------------------
-// Common Modules
-//-------------------------------------------------------------------------------
-
-var bugpack = require('bugpack').context();
-
-
-//-------------------------------------------------------------------------------
-// BugPack
-//-------------------------------------------------------------------------------
-
-var Class                       = bugpack.require('Class');
-var BoxView                     = bugpack.require('airbug.BoxView');
-var ButtonView                  = bugpack.require('airbug.ButtonView');
-var ButtonViewEvent             = bugpack.require('airbug.ButtonViewEvent');
-var CopyToClipboardButtonView   = bugpack.require('airbug.CopyToClipboardButtonView');
-var DropdownItemView            = bugpack.require('airbug.DropdownItemView');
-var DropdownViewEvent           = bugpack.require('airbug.DropdownViewEvent');
-var FauxTextAreaView            = bugpack.require('airbug.FauxTextAreaView');
-var IconView                    = bugpack.require('airbug.IconView');
-var ParagraphView               = bugpack.require('airbug.ParagraphView');
-var TextView                    = bugpack.require('airbug.TextView');
-var AutowiredAnnotation         = bugpack.require('bugioc.AutowiredAnnotation');
-var PropertyAnnotation          = bugpack.require('bugioc.PropertyAnnotation');
-var BugMeta                     = bugpack.require('bugmeta.BugMeta');
-var CarapaceContainer           = bugpack.require('carapace.CarapaceContainer');
-var ViewBuilder                 = bugpack.require('carapace.ViewBuilder');
-var ZeroClipboard               = bugpack.require('zeroclipboard.ZeroClipboard');
-
-
-//-------------------------------------------------------------------------------
-// Simplify References
-//-------------------------------------------------------------------------------
-
-var autowired   = AutowiredAnnotation.autowired;
-var bugmeta     = BugMeta.context();
-var property    = PropertyAnnotation.property;
-var view        = ViewBuilder.view;
-
-
-//-------------------------------------------------------------------------------
-// Declare Class
-//-------------------------------------------------------------------------------
-
-var AddRoomMemberContainer = Class.extend(CarapaceContainer, {
-
-    //-------------------------------------------------------------------------------
-    // Constructor
-    //-------------------------------------------------------------------------------
-
-    _constructor: function(roomModel) {
-
-        this._super();
-
-
-        //-------------------------------------------------------------------------------
-        // Declare Variables
-        //-------------------------------------------------------------------------------
-
-        /**
-         * @private
-         * @type {RoomModel}
-         */
-        this.roomModel          = roomModel;
-
-        /**
-         * @private
-         * @type {zeroclipboard.ZeroClipboard}
-         */
-        this.clip               = null;
-
-        // Modules
-        //-------------------------------------------------------------------------------
-
-        /**
-         * @private
-         * @type {NavigationModule}
-         */
-        this.navigationModule   = null;
-
-        /**
-         * @private
-         * @type {WindowUtil}
-         */
-        this.windowUtil         = null;
-
-
-
-        // Views
-        //-------------------------------------------------------------------------------
-
-        /**
-         * @private
-         * @type {DropdownItemView}
-         */
-        this.dropdownItemView   = null;
-
-        /**
-         * @private
-         * @type {ButtonView}
-         */
-        this.copyLinkButton     = null;
-    },
-
-
-    //-------------------------------------------------------------------------------
-    // CarapaceContainer Methods
-    //-------------------------------------------------------------------------------
-
-    /**
-     * @protected
-     */
-    activateContainer: function() {
-        this._super();
-        var fauxTextArea = this.dropdownItemView.$el.find(".faux-textarea p");
-        fauxTextArea.on("click", function() {
-            fauxTextArea.selectText();
-        });
-    },
-
-    /**
-     * @protected
-     */
-    createContainer: function() {
-        this._super();
-
-        // Create Views
-        //-------------------------------------------------------------------------------
-
-        var currentUrl = this.windowUtil.getUrl();
-
-        this.dropdownItemView =
-            view(DropdownItemView)
-                .children([
-                    //TODO BRN: This needs to be encapsulated in it's own view so that it can react to model changes
-                    view(BoxView)
-                        .children([
-                            view(ParagraphView)
-                                .attributes({text: "Share room " + this.roomModel.getProperty("name")}),
-                            view(FauxTextAreaView)
-                                .children([
-                                    view(ParagraphView)
-                                    .attributes({
-                                        text: currentUrl + "#room/" + this.roomModel.getProperty("id")
-                                    })
-                                ]),
-                            view(CopyToClipboardButtonView)
-                                .attributes({type: "primary", align: "right", size: ButtonView.Size.NORMAL})
-                                .children([
-                                    view(TextView)
-                                        .attributes({text: " Copy Link"})
-                                        .appendTo('*[id|="button"]'),
-                                    view(IconView)
-                                        .attributes({
-                                            type: IconView.Type.PAPERCLIP,
-                                            color: IconView.Color.WHITE})
-                                        .appendTo('*[id|="button"]')
-                                ])
-                        ])
-                ])
-            .build();
-
-        // Wire Up Views
-        //-------------------------------------------------------------------------------
-
-        this.setViewTop(this.dropdownItemView);
-    },
-
-    /**
-     * @protected
-     */
-    createContainerChildren: function() {
-        this._super();
-        //TODO
-    },
-
-    /**
-     * @protected
-     */
-    deinitializeContainer: function() {
-        this._super();
-        this.destroyZeroClipboard();
-        this.dropdownItemView.removeEventListener(DropdownViewEvent.EventType.DROPDOWN_SELECTED, this.hearDropdownItemClickedEvent, this);
-    },
-
-    /**
-     * @protected
-     */
-    initializeContainer: function() {
-        this._super();
-        this.createZeroClipboard();
-        this.dropdownItemView.addEventListener(DropdownViewEvent.EventType.DROPDOWN_SELECTED, this.hearDropdownItemClickedEvent, this);
-    },
-
-
-    //-------------------------------------------------------------------------------
-    // Private Methods
-    //-------------------------------------------------------------------------------
-
-    /**
-     * @private
-     */
-    createZeroClipboard: function() {
-        var button      = this.getViewTop().$el.find('.btn')[0];
-        var currentUrl  = this.windowUtil.getUrl();
-        var copyText    = currentUrl + "#room/" + this.roomModel.getProperty("id");
-        var options     = {
-            moviePath: "/zeroclipboard/ZeroClipboard.swf"
-        };
-
-        this.clip        = new ZeroClipboard(button, options);
-        var clip = this.clip;
-
-        clip.on( 'dataRequested',   function(client, args) {
-            clip.setText(copyText);
-        });
-
-        clip.on( 'load',            function(client, args) {
-        });
-
-        clip.on( 'complete',        function(client, args) {
-        });
-
-        clip.on( 'mouseover',       function(client, args) {
-        });
-
-        clip.on( 'mouseout',        function(client, args) {
-        });
-
-        clip.on( 'mousedown',       function(client, args) {
-        });
-
-        clip.on( 'mouseup',         function(client, args) {
-        });
-
-        clip.on( 'noflash',         function(client, args) {
-        });
-
-        clip.on( 'wrongflash',      function(client, args) {
-        });
-
-        // botton.on("resize", function(event){
-        //     clip.reposition();
-        // });
-    },
-
-    /**
-     * @private
-     */
-    destroyZeroClipboard: function() {
-        this.clip = null;
-    },
-
-
-    //-------------------------------------------------------------------------------
-    // Event Listeners
-    //-------------------------------------------------------------------------------
-
-    /**
-     * @private
-     * @param {DropdownViewEvent} event
-     */
-    hearDropdownItemClickedEvent: function(event) {
-
-    }
-});
-
-
-//-------------------------------------------------------------------------------
-// BugMeta
-//-------------------------------------------------------------------------------
-
-bugmeta.annotate(AddRoomMemberContainer).with(
-    autowired().properties([
-        property("navigationModule").ref("navigationModule"),
-        property("windowUtil").ref("windowUtil")
-    ])
-);
-
-
-//-------------------------------------------------------------------------------
-// Exports
-//-------------------------------------------------------------------------------
-
-bugpack.export("airbug.AddRoomMemberContainer", AddRoomMemberContainer);

projects/airbugclient/js/src/containers/pages/roompage/shareroom/ShareRoomButtonContainer.js
@@ -141,6 +141,23 @@ var ShareRoomButtonContainer = Class.extend(ButtonContainer, {
      */
     initializeContainer: function() {
         this._super();
+        this.buttonView.addEventListener(ButtonViewEvent.EventType.CLICKED, this.hearButtonClickedEvent, this);
+    },
+
+    /**
+     * @protected
+     */
+    deinitializeContainer: function() {
+        this._super();
+        this.buttonView.removeEventListener(ButtonViewEvent.EventType.CLICKED, this.hearButtonClickedEvent, this);
+    },
+
+    /**
+     * @private
+     */
+    hearButtonClickedEvent: function(event) {
+        console.log("ShareRoomButtonContainer#hearButtonClickedEvent");
+        this.commandModule.relayCommand(CommandType.DISPLAY.SHARE_ROOM_OVERLAY, {});
     }
 });
 

commit 3980994c5dda02660e24e240010d3d8b0fed2525

BugFix. Fixes bug that broke workspace tray and hamburger buttons. (A bug that was introduced in f8debe0849ab561ab0771c5843a9246239e844e4)

projects/airbugclient/js/src/containers/pages/PageContainer.js
@@ -156,6 +156,7 @@ var PageContainer = Class.extend(ApplicationContainer, {
      */
     initializeContainer: function() {
         this._super();
+        this.initializeEventListeners();
         this.initializeCommandSubscriptions();
     },
 
@@ -164,6 +165,7 @@ var PageContainer = Class.extend(ApplicationContainer, {
      */
     deinitializeContainer: function() {
         this._super();
+        this.deinitializeEventListeners();
         this.deinitializeCommandSubscriptions();
     },
 
@@ -171,17 +173,25 @@ var PageContainer = Class.extend(ApplicationContainer, {
     // Private Instance Methods
     //-------------------------------------------------------------------------------
 
+    initializeEventListeners: function() {
+
+    },
+
+    deinitializeEventListeners: function() {
+
+    },
+
     /**
      * @private
      */
     initializeCommandSubscriptions: function() {
-        this.commandModule.subscribe(CommandType.HIDE.WORKSPACE,        this.handleHideWorkspaceCommand,        this);
+        this.commandModule.subscribe(CommandType.HIDE.WORKSPACE,         this.handleHideWorkspaceCommand,       this);
         this.commandModule.subscribe(CommandType.TOGGLE.WORKSPACE,       this.handleToggleWorkspaceCommand,     this);
         this.commandModule.subscribe(CommandType.TOGGLE.HAMBURGER_LEFT,  this.handleToggleHamburgerLeftCommand, this);
     },
 
     deinitializeCommandSubscriptions: function() {
-        this.commandModule.unsubscribe(CommandType.HIDE.WORKSPACE,        this.handleHideWorkspaceCommand,        this);
+        this.commandModule.unsubscribe(CommandType.HIDE.WORKSPACE,         this.handleHideWorkspaceCommand,        this);
         this.commandModule.unsubscribe(CommandType.TOGGLE.WORKSPACE,       this.handleToggleWorkspaceCommand,      this);
         this.commandModule.unsubscribe(CommandType.TOGGLE.HAMBURGER_LEFT,  this.handleToggleHamburgerLeftCommand,  this);
     },

projects/airbugclient/js/src/containers/pages/roompage/RoomPageContainer.js
@@ -212,32 +212,32 @@ var RoomPageContainer = Class.extend(PageContainer, {
 
     initializeContainer: function() {
         this._super();
-        this.initializeEventListeners();
-        this.initializeCommandSubscriptions();
     },
 
     deinitializeContainer: function() {
         this._super();
-        this.deinitializeEventListeners();
-        this.deinitializeCommandSubscriptions();
     },
 
     initializeEventListeners: function() {
+        this._super();
         var overlayView  = this.shareRoomContainer.getViewTop();
         overlayView.addEventListener(ButtonViewEvent.EventType.CLICKED, this.hearOverlayBackgroundClickedEvent, this);
     },
 
     deinitializeEventListeners: function() {
+        this._super();
         var overlayView  = this.shareRoomContainer.getViewTop();
         overlayView.removeEventListener(ButtonViewEvent.EventType.CLICKED, this.hearOverlayBackgroundClickedEvent, this);
     },
 
     initializeCommandSubscriptions: function() {
+        this._super();
         this.commandModule.subscribe(CommandType.DISPLAY.SHARE_ROOM_OVERLAY, this.handleDisplayShareRoomOverlayCommand, this);
         this.commandModule.subscribe(CommandType.HIDE.SHARE_ROOM_OVERLAY, this.handleHideShareRoomOverlayCommand, this);
     },
 
     deinitializeCommandSubscriptions: function() {
+        this._super();
         this.commandModule.unsubscribe(CommandType.DISPLAY.SHARE_ROOM_OVERLAY, this.handleDisplayShareRoomOverlayCommand, this);
         this.commandModule.unsubscribe(CommandType.HIDE.SHARE_ROOM_OVERLAY, this.handleHideShareRoomOverlayCommand, this);
     },