e5396bb1893d8eafad83f36ef7c07d98c81eb034
Sung Choi <m.sung.choi@gmail.com>
2014-01-09 18:39:35 -0800

    Update. Updates AssetManagerModule to use module annotations and autoload.

---
+++ projects/airbugclient/js/src/modules/AssetManagerModule.js
@@ -0,0 +1,117 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbug')
+//@Autoload
+
+//@Export('AssetManagerModule')
+
+//@Require('Class')
+//@Require('airbug.ManagerModule')
+//@Require('bugioc.ArgAnnotation')
+//@Require('bugioc.ModuleAnnotation')
+//@Require('bugmeta.BugMeta')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack         = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// BugPack
+//-------------------------------------------------------------------------------
+
+var Class           = bugpack.require('Class');
+var ManagerModule   = bugpack.require('airbug.ManagerModule');
+var ArgAnnotation                   = bugpack.require('bugioc.ArgAnnotation');
+var ModuleAnnotation                = bugpack.require('bugioc.ModuleAnnotation');
+var BugMeta                         = bugpack.require('bugmeta.BugMeta');
+
+
+//-------------------------------------------------------------------------------
+// Simplify References
+//-------------------------------------------------------------------------------
+
+var arg                             = ArgAnnotation.arg;
+var bugmeta                         = BugMeta.context();
+var module                          = ModuleAnnotation.module;
+
+
+//-------------------------------------------------------------------------------
+// Declare Class
+//-------------------------------------------------------------------------------
+
+var AssetManagerModule = Class.extend(ManagerModule, {
+
+
+    //-------------------------------------------------------------------------------
+    // Public Methods
+    //-------------------------------------------------------------------------------
+
+    /**
+     * @param {string} url
+     * @param {function(Throwable, Meld=)} callback
+     */
+    addAssetFromUrl: function(url, callback) {
+        var requestData = {url: url};
+        this.request("addAssetFromUrl", requestData, callback);
+    },
+
+    /**
+     * @param {{
+     *      name: string
+     * }} roomObject
+     * @param {function(Throwable, Meld=)} callback
+     */
+    createAsset: function(assetObject, callback) {
+        this.create("Asset", assetObject, callback);
+    },
+
+    /**
+     * @param {Object=} assetObject
+     * @param {MeldDocument=} assetMeldDocument
+     * @returns {AssetModel}
+     */
+//    generateAssetModel: function(assetObject, assetMeldDocument) {
+//        return new AssetModel(assetObject, assetMeldDocument);
+//    },
+
+    /**
+     * @param {string} roomId
+     * @param {function(Throwable, Meld=)} callback
+     */
+    retrieveAsset: function(assetId, callback) {
+        this.retrieve("Asset", assetId, callback);
+    },
+
+    /**
+     * @param {Array.<string>} roomIds
+     * @param {function(Throwable, Map.<string, Meld>=)} callback
+     */
+    retrieveAssets: function(assetIds, callback) {
+        this.retrieveEach("Asset", assetIds, callback);
+    }
+});
+
+//-------------------------------------------------------------------------------
+// BugMeta
+//-------------------------------------------------------------------------------
+
+bugmeta.annotate(AssetManagerModule).with(
+    module("chatMessageManagerModule")
+        .args([
+            arg().ref("airbugApi"),
+            arg().ref("meldStore"),
+            arg().ref("meldBuilder")
+        ])
+);
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export("airbug.AssetManagerModule", AssetManagerModule);
