2d8b95b62a3a64ec80f3dc8822ec0544d52bf2e6
Sung Choi <m.sung.choi@gmail.com>
2014-01-15 08:28:37 -0800

    Update. Temp. Adds a link to the full size image from the MessageContentImageView.

--- projects/airbugclient/js/src/views/chatmessages/messages/MessageContentImageView.js
+++ projects/airbugclient/js/src/views/chatmessages/messages/MessageContentImageView.js
@@ -36,10 +36,10 @@ var MessageContentImageView = Class.extend(MustacheView, {
     //-------------------------------------------------------------------------------
 
     template:   '<div id="message-image-{{cid}}" class="message-image image-preview">' +
-            '<a> <img id="image-{{cid}}" src={{midsizeUrl}} /> </a>' +
+            '<a href="{{}}"> <img id="image-{{cid}}" src={{midsizeUrl}} /> </a>' +
         '</div>',
 
-
+        //NOTE: SUNG Temporarily using an href for the full size image until we have implemented our own full size view.
     //-------------------------------------------------------------------------------
     // CarapaceView Extensions
     //-------------------------------------------------------------------------------
@@ -49,8 +49,9 @@ var MessageContentImageView = Class.extend(MustacheView, {
      */
     generateTemplateData: function() {
         var data = this._super();
-        data.thumbnailUrl = data.model.thumbnailUrl; //CLEANUP create helper methods.
-        data.midsizeUrl = data.model.midsizeUrl;
+        data.midsizeUrl     = data.model.midsizeUrl;
+        data.thumbnailUrl   = data.model.thumbnailUrl;
+        data.url            = data.model.url;
         return data;
     },
 
@@ -64,6 +65,9 @@ var MessageContentImageView = Class.extend(MustacheView, {
         switch (propertyName) {
             case "midsizeUrl":
                 this.findElement('#image-' + this.getCid()).attr("src", propertyValue);
+                break;
+            case "url":
+                this.findElement('#message-image-' + this.getCid() + ">a").attr("href", propertyValue);
                 break;
         }
     }
