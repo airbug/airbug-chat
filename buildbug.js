83c81e2a63e0ff2b4631ad94aaf6db8fe3ed0eeb
Brian Neisler <neisler.brian@gmail.com>
2014-01-17 13:09:16 -0800

    Bump version to 0.0.8

--- buildbug.js
+++ buildbug.js
@@ -34,7 +34,7 @@ var nodejs              = enableModule('nodejs');
 // Values
 //-------------------------------------------------------------------------------
 
-var version             = "0.0.7";
+var version             = "0.0.8";
 var dependencies        = {
     "aws-sdk": "1.17.1",
     bugpack: "https://s3.amazonaws.com/airbug/bugpack-0.0.5.tgz",
