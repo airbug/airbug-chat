f139e4737f3e7e8da695e1312179ae6fdbc2e584
Sung Choi <m.sung.choi@gmail.com>
2014-01-23 18:43:21 -0800

    Update. Uncomments out UserAssetServiceTests. Fixes the tests that expected a map instead of a list and outlined a new test for #retrieveUserImageAssetsByUserId. Refactor. Pulled out the UserAssetPusherStub and UserImageAssetStreamPusherStub into their own files and classes.

---
+++ projects/airbugserver/js/test/stubs/UserAssetPusherStub.js
@@ -0,0 +1,54 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbugservertest')
+
+//@Export('UserAssetPusherStub')
+
+//@Require('Class')
+//@Require('Obj')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack                 = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// Bugpack Modules
+//-------------------------------------------------------------------------------
+
+var Class                   = bugpack.require('Class');
+var Obj                     = bugpack.require('Obj');
+
+
+//-------------------------------------------------------------------------------
+// Stub
+//-------------------------------------------------------------------------------
+
+var UserAssetPusherStub = Class.extend(Obj, {
+    meldCallWithUserAsset: function(callUuid, userAsset, callback) {
+        callback(undefined);
+    },
+    meldCallWithUserAssets: function(callUuid, userAssets, callback) {
+        callback(undefined);
+    },
+    pushUserAssetToCall: function(userAsset, callUuid, callback) {
+        callback(undefined);
+    },
+    pushUserAssetsToCall: function(userAsset, callUuid, callback) {
+        callback(undefined);
+    },
+    streamUserImageAsset: function(userImageAssetStream, userImageAsset, callback) {
+        callback(undefined);
+    }
+});
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export('airbugservertest.UserAssetPusherStub', UserAssetPusherStub);
---
+++ projects/airbugserver/js/test/stubs/UserImageAssetStreamPusherStub.js
@@ -0,0 +1,41 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbugservertest')
+
+//@Export('UserImageAssetStreamPusherStub')
+
+//@Require('Class')
+//@Require('Obj')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack                 = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// Bugpack Modules
+//-------------------------------------------------------------------------------
+
+var Class                   = bugpack.require('Class');
+var Obj                     = bugpack.require('Obj');
+
+//-------------------------------------------------------------------------------
+// Stub
+//-------------------------------------------------------------------------------
+
+var UserImageAssetStreamPusherStub = Class.extend(Obj, {
+    pushUserImageAssetStreamToCall: function(userImageAssetStream, callUuid, callback) {
+        callback(undefined);
+    }
+});
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export('airbugservertest.UserImageAssetStreamPusherStub', UserImageAssetStreamPusherStub);
