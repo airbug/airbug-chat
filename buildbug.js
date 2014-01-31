1746846021465a1c788aba837ccf54ff8298458e
Brian Neisler <neisler.brian@gmail.com>
2014-01-30 23:33:50 -0800

    Update airbug version to 0.0.11

--- buildbug.js
+++ buildbug.js
@@ -34,7 +34,7 @@ var nodejs              = enableModule('nodejs');
 // Values
 //-------------------------------------------------------------------------------
 
-var version             = "0.0.10";
+var version             = "0.0.11";
 var dependencies        = {
     "aws-sdk": "1.17.1",
     bugpack: "https://s3.amazonaws.com/airbug/bugpack-0.0.5.tgz",
