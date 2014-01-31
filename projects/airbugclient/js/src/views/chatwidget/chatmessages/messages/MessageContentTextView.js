ae84171e8c3bfec942e76e3441e4a024d161530a
Brian Neisler <neisler.brian@gmail.com>
2014-01-30 22:55:41 -0800

    Auto convert gist links to gist embeds [Finishes story #64882248]

--- projects/airbugclient/js/src/views/chatwidget/chatmessages/messages/MessageContentTextView.js
+++ projects/airbugclient/js/src/views/chatwidget/chatmessages/messages/MessageContentTextView.js
@@ -8,23 +8,27 @@
 
 //@Require('Class')
 //@Require('HtmlUtil')
+//@Require('IdGenerator')
 //@Require('airbug.MustacheView')
+//@Require('jquery.JQuery')
 
 
 //-------------------------------------------------------------------------------
 // Common Modules
 //-------------------------------------------------------------------------------
 
-var bugpack = require('bugpack').context();
+var bugpack                 = require('bugpack').context();
 
 
 //-------------------------------------------------------------------------------
 // BugPack
 //-------------------------------------------------------------------------------
 
-var Class           = bugpack.require('Class');
-var HtmlUtil        = bugpack.require('HtmlUtil');
-var MustacheView    = bugpack.require('airbug.MustacheView');
+var Class                   = bugpack.require('Class');
+var HtmlUtil                = bugpack.require('HtmlUtil');
+var IdGenerator             = bugpack.require('IdGenerator');
+var MustacheView            = bugpack.require('airbug.MustacheView');
+var JQuery                  = bugpack.require('jquery.JQuery');
 
 
 //-------------------------------------------------------------------------------
@@ -86,12 +90,6 @@ var MessageContentTextView = Class.extend(MustacheView, {
         var html = HtmlUtil.stringToHtml(text);
         html = this.replaceUrls(html);
         return html;
-
-        /*http://www.youtube.com/watch?v=9LUv3kbmNfg
-
-        <iframe id="ytplayer" type="text/html" width="640" height="360"
-        src="https://www.youtube.com/embed/M7lc1UVf-VE"
-        frameborder="0" allowfullscreen>*/
     },
 
     /**
@@ -105,6 +103,38 @@ var MessageContentTextView = Class.extend(MustacheView, {
                 var v = url.getUrlQuery("v");
                 return '<br/><iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/' +
                     v + '" frameborder="0" allowfullscreen></iframe><br/>';
+            } else if (url.getHost() === "gist.github.com" && !!url.getPath().match(/\/\w.*\/[0-9].*/)) {
+
+                var iframeId = "gistFrame-" + IdGenerator.generateId();
+
+                // HOLY HACKS BATMAN!
+                setTimeout(function() {
+                    var gistFrame = JQuery("#" + iframeId)[0];
+                    var gistFrameHTML = '<html>' +
+                            '<body>' +
+                                '<script type="text/javascript" src="' + match + '.js"></script>' +
+                                '<script>' +
+                                    'var newHeight = document.body.scrollHeight;' +
+                                    'var i = parent.document.getElementById("' + iframeId + '");' +
+                                    'i.style.height = parseInt(newHeight) + "px";' +
+                                '</script>' +
+                            '</body>' +
+                        '</html>';
+                    var gistFrameDoc = gistFrame.document;
+
+                    if (gistFrame.contentDocument) {
+                        gistFrameDoc = gistFrame.contentDocument;
+                    } else if (gistFrame.contentWindow) {
+                        gistFrameDoc = gistFrame.contentWindow.document;
+                    }
+
+                    gistFrameDoc.open();
+                    gistFrameDoc.writeln(gistFrameHTML);
+                    gistFrameDoc.close();
+                }, 0);
+
+                return '<br/><iframe id="' + iframeId + '" class="gist-embed" width="100%"></iframe><br/>';
+                //return '<br/><iframe src="' + match + '.pibb?scroll=true"></iframe><br/>';
             }
             return '<a href="' + url.toString() + '">' + match + '</a>';
         });
