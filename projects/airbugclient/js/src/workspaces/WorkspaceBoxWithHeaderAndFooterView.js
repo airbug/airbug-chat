90fb8be70ecb35b5fdc74f9f970f4dd54449298e
Sung Choi <m.sung.choi@gmail.com>
2014-03-08 23:28:12 -0800

    New. Adds WorkspaceBoxWithHeaderAndFooterView.js

---
+++ projects/airbugclient/js/src/workspaces/WorkspaceBoxWithHeaderAndFooterView.js
@@ -0,0 +1,53 @@
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
+
+//@Package('airbug')
+
+//@Export('WorkspaceBoxWithHeaderAndFooterView')
+
+//@Require('Class')
+//@Require('airbug.MustacheView')
+
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var bugpack = require('bugpack').context();
+
+
+//-------------------------------------------------------------------------------
+// BugPack
+//-------------------------------------------------------------------------------
+
+var Class           = bugpack.require('Class');
+var MustacheView    = bugpack.require('airbug.MustacheView');
+
+
+//-------------------------------------------------------------------------------
+// Declare Class
+//-------------------------------------------------------------------------------
+
+var WorkspaceBoxWithHeaderAndFooterView = Class.extend(MustacheView, {
+
+    //-------------------------------------------------------------------------------
+    // Template
+    //-------------------------------------------------------------------------------
+
+    template:   '<div id="box-{{cid}}" class="workspace-widget box box-with-header box-with-footer {{classes}}">' +
+        '<div id="box-header-{{cid}}" class="workspace-widget-header box-header">' +
+        '</div>' +
+        '<div id="box-body-{{cid}}" class="workspace-widget-body box-body">' +
+        '</div>' +
+        '<div id="box-footer-{{cid}}" class="workspace-widget-footer box-footer">' +
+        '</div>' +
+        '</div>'
+});
+
+
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
+
+bugpack.export("airbug.WorkspaceBoxWithHeaderAndFooterView", WorkspaceBoxWithHeaderAndFooterView);
