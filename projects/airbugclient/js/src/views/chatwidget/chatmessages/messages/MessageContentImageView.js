794b4935e6d94f0fbe2dac006cf05a2fcbb9560d
Sung Choi <m.sung.choi@gmail.com>
2014-03-12 18:23:26 -0700

    Update. Adds save to image list button to MessageContentImageView. Removes message-content wrapper div from template.

--- projects/airbugclient/js/src/views/chatwidget/chatmessages/messages/MessageContentImageView.js
+++ projects/airbugclient/js/src/views/chatwidget/chatmessages/messages/MessageContentImageView.js
@@ -8,6 +8,7 @@
 
 //@Require('Class')
 //@Require('airbug.MustacheView')
+//@Require('airbug.ImageViewEvent')
 
 
 //-------------------------------------------------------------------------------
@@ -23,6 +24,7 @@ var bugpack = require('bugpack').context();
 
 var Class           = bugpack.require('Class');
 var MustacheView    = bugpack.require('airbug.MustacheView');
+var ImageViewEvent  = bugpack.require('airbug.ImageViewEvent');
 
 
 //-------------------------------------------------------------------------------
@@ -38,16 +40,28 @@ var MessageContentImageView = Class.extend(MustacheView, {
     //NOTE: SUNG Temporarily using an href for the full size image until we have implemented our own full size view.
 
     template:
-        '<div id="message-content-{{cid}}" class="message-image image-preview">' +
-            '<a id="image-link-{{cid}}" href="{{model.url}}"  target="_blank"> ' +
+        '<div id="message-image-{{cid}}" class="message-image image-preview">' +
+            '<a id="image-link-{{cid}}" href="{{model.url}}" target="_blank"> ' +
                 '<img id="image-{{cid}}" src={{model.midsizeUrl}} />' +
             '</a>' +
+            '<div class="btn-toolbar">' +
+                '<div class="btn-group">' +
+                    '<button class="btn btn-link"><span>Save to Image List</span></button>' +
+                '</div>' +
+            '</div>' +
         '</div>',
 
 
     //-------------------------------------------------------------------------------
     // Getters and Setters
     //-------------------------------------------------------------------------------
+
+    /**
+     * @return {string}
+     */
+    getImageAssetId: function() {
+        return this.model.getProperty("assetId");
+    },
 
     /**
      * @return {$}
@@ -66,14 +80,40 @@ var MessageContentImageView = Class.extend(MustacheView, {
     /**
      * @return {$}
      */
-    getMessageContentElement: function() {
-        return this.findElement("#message-content-{{cid}}");
+    getImageSaveButtonElement: function() {
+        return this.findElement(".btn-toolbar > .btn-group:last-child > button:last-child");
+    },
+
+    /**
+     * @return {$}
+     */
+    getMessageImageElement: function() {
+        return this.findElement("message-image-{{cid}}");
     },
 
 
     //-------------------------------------------------------------------------------
     // CarapaceView Methods
     //-------------------------------------------------------------------------------
+
+    /**
+     * @protected
+     */
+    initializeView: function() {
+        var _this = this;
+        this._super();
+        this.getImageSaveButtonElement().on("click", function(event){
+            _this.handleSaveButtonClick(event);
+        });
+    },
+
+    /**
+     * @protected
+     */
+    deinitializeView: function() {
+        this._super();
+        this.getImageSaveButtonElement().off();
+    },
 
     /**
      * @protected
@@ -90,6 +130,15 @@ var MessageContentImageView = Class.extend(MustacheView, {
                 this.getImageLinkElement().attr("href", propertyValue);
                 break;
         }
+    },
+
+    /**
+     * @private
+     */
+    handleSaveButtonClick: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.dispatchEvent(new ImageViewEvent(ImageViewEvent.EventType.CLICKED_SAVE, {assetId: this.getImageAssetId()}));
     }
 });
 
